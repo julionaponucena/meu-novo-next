c24dcc2623adff85a2c5120f7ea72baf7b8573cd
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-04 16:39:04 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-   <a href="teste.js">Teste</a>
+   <Link href="teste.js">
+   <a>Teste</a>
+   </Link>
   )
 }
