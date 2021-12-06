724b81d581bf396699d2fd3c604a4599dccb4c98
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-05 22:58:34 -0300

    Create menu-itens.js

---
+++ scripts/menu-itens.js
@@ -0,0 +1,252 @@
+
+
+
+
+
+
+
+
+
+var Artigo = [{
+ img: 'vitima.jpg',
+ titulo: "NÃO SEJA VITIMADO PELO DETERMINISMO EXISTENCIAL",
+ link:"psicanalise-2.php"
+},
+{
+ img:'crianca-feliz.jpg',
+ titulo: "COLABORAÇÃO PARA O BEM-ESTAR COLETIVO E AUTORREALIZAÇÃO",
+ link:'psicanalise-3.php'
+},
+{img:'ansiedade.jpg',
+titulo:"TERAPIA COM PSICANÁLISE  PARA ENFRENTAR AS DIFICULDADES EMOCIONAIS",
+link:'psicanalise-4.php'
+},
+{
+    img:'pintura.jpg',
+    titulo:"PSICANÁLISE: CAMINHO PARA A RESSIGNIFICAÇÃO DO SOFRIMENTO",
+    link:"psicanalise-6.php"
+},
+{ 
+    img:'gato-sorte.jpg',
+    titulo:"COMO SENTIR QUE VOCÊ TEM VALOR",
+    link:"psicanalise-7.php"
+},
+{
+    img:'autovalor',
+    titulo:"REFLEXÃO SOBRE OS TRAUMAS E NOSSA CONDIÇÃO EXISTENCIAL",
+    link:"psicanalise-8.php"
+},
+{
+    img:'contemplacao',
+    titulo:"MENTES SADIAS GERAM  CORPOS SAUDÁVEIS",
+    link:'psicanalise-9.php'
+},
+{
+    img:'reflexão',
+    titulo:"BREVE REFLEXÃO SOBRE A REALIZAÇÃO HUMANA",
+    link:'psicanalise-10.php'
+},
+{ 
+    img:'luto',
+    titulo:'A DOR DO LUTO',
+    link:"psicanalise-11.php"
+},
+{
+    img:'perdao',
+    titulo:'PERDÃO E AUTOPERDÃO: SEUS BENEFÍCIOS PARA A SAÚDE FÍSICA E MENTAL',
+    link:'psicanalise-12.php'
+},
+{
+    img:'escuro.jpg',
+    titulo:"A CEGUEIRA PROVOCADA PELO DESEJO DE NÃO ENXERGAR: VISÃO PSICANALÍTICA DA NEGAÇÃO:",
+    link:'psicanalise-13.php'
+},
+{
+    img:'inferior.jpg',
+    titulo:'COMPLEXO DE INFERIORIDADE: AUTOAFIRMAÇÃO NÃO, AUTOACEITAÇÃO SIM',
+    link:'psicanalise-14.php'
+},
+{ 
+    img:'relogio.jpg',
+    titulo:'RELAÇÃO ENTRE PASSADO E PRESENTE NA TERAPIA PSICANALÍTICA',
+    link:'psicanalise-15.php'
+},
+{
+    img:'educacao.jpeg',
+    titulo:'PRATIQUE EDUCAÇÃO COM BASE NO ENCORAJAMENTO',
+    link:'psicanalise-16.php'
+},
+{
+    img:'critica.jpg',
+    titulo:"SUPERIORIDADE DISFARÇADA EM FORMA DE CRÍTICAS E  ELOGIOS",
+    link:'psicanalise-17.php'
+},
+{
+    img:'pessoas.jpg',
+    titulo:'IDENTIFIQUE A CAUSA DOS PROBLEMAS  DE RELACIONAMENTOS INTERPESSOAIS.php',
+    link:'psicanalise-18.php',
+},
+{
+    img:'reconhecimento.png',
+    titulo:'LIVRAR-SE DA NECESSIDADE DE RECONHECIMENTO PODE SER LIBERTADOR',
+    link:'psicanalise-19.php'
+},
+{
+    img:'festa.jpg',
+    titulo:'O SOFRIMENTO A PARTIR DOS RELACIONAMENTOS INTERPESSOAIS',
+    link:'psicanalise-20.php',
+},
+{
+    img:'armadilha',
+    titulo:'NÃO PERMITA QUE SUA VIDA SEJA DETERMINADA PELAS “ARMADILHAS CONCEITUAIS”',
+    link:'psicanalise-21.php'
+},
+{ 
+    img:'boneco-triste',
+    titulo:'DEPRESSÃO E FALTA DE SENTIDO EXISTENCIAL',
+    link:'psicanalise-22.php'
+},
+{
+    img:'interrogação.jpg',
+    titulo:'DIMINUA A ANSIEDADE PARATICANDO A GESTÃO DE INCERTEZAS',
+    link:'psicanlise-23.php'
+},
+{
+    img:'oceano.jpg',
+    titulo:'ADMITIR O ERRO NÃO É SINAL DE FRAQUEZA E MUITO MENOS DE DERROTA',
+    link:'psicanalise-24.php'
+},
+{
+    img:'estrelas.jpg',
+    titulo:'PREOCUPAÇÃO EXAGERADA COM A IMAGEM EXTERIOR PODE GERAR CONFLITO INTERIOR',
+    link:'psicanalise-25.php'
+},
+{
+    img:'significar.jpg',
+    titulo:'RESSIGNIFIQUE SEU OLHAR DIANTE DA VIDA E SE LIVRE DA COMPETIÇÃO COMPULSIVA',
+    link:'psicanalise-26.php'
+},
+{ 
+    img:'boxe.jpg',
+    titulo:'POR QUE TRANSFORMAMOS A VIDA EM UMA GRANDE COMPETIÇÃO?',
+    link:'psicanalise-27.php'
+},
+{
+    img:'rei.jpg',
+    titulo:'RELAÇÃO ENTRE ARROGÂNCIA E COMPLEXO DE INFERIORIDADE',
+    link:'psicanalise-28.php'
+},
+{
+    img:'gatinho.jpg',
+    titulo:'USE O SENTIMENTO DE INFERIORIDADE COMO SEU ALIADO',
+    link:'psicanalise-29.php'
+},
+{
+    img:'psicoterapia.jpg',
+    titulo:'A PRÁTICA DA PSICOTERAPIA E O CAMINHO DA CURA',
+    link:'psicanalise-30.php'
+},
+{
+    img:'coracao-partido.jpg',
+    titulo:'O QUE EXISTE DE OBJETIVO NOS SENTIMENTOS DE INFERIORIDADE?',
+    link:'psicanalise-31.php'
+},
+{
+    img:'homem-ceu.jpg',
+    titulo:'POR QUE MUITAS PESSOAS SE MALTRATAM APOSTANDO EM SEUS PRÓPRIOS "DEFEITOS"?',
+    link:'psicanalise-32.php'
+},
+{
+    img:'caminho.jpg',
+    titulo:'SUA VIDA É DECIDIDA AQUI E AGORA',
+    link:'psicanalise-33.php'
+},
+{
+    img:'mudanca',
+    titulo:'POR QUE MUITOS OPTAM POR NÃO MUDAR?',
+    link:'psicanalise-34.php'
+},
+{ 
+    img:'psicoterapia-online',
+    titulo:'Terapia Online para adultos, adolescentes e casais.',
+    link:'psicanalise-35.php'
+},
+{
+    img:'rocks',
+    titulo:'VOCÊ SE SENTE BEM DO JEITO QUE ESTÁ?',
+    link:'psicanalise-36.php'
+},
+{
+    img:'raiva',
+    titulo:'COMO LIDAR COM O SENTIMENTO DA RAIVA?',
+    link:'psicanalise-37.php'
+},
+{ 
+    img:'relogio-2',
+    titulo:'É POSSÍVEL VIVER SEM SER CONTROLADO PELO PASSADO?',
+    link:'psicanalise-38.php'
+},
+{
+    img:'borboleta.jpg',
+    titulo:'REFLEXÃO SOBRE OS TRAUMAS E NOSSA CONDIÇÃO EXISTENCIAL',
+    link:'psicanalise-39.php'
+},
+{
+    img:'praia.jpg',
+    titulo:'RELAÇÃO ENTRE PASSADO E PRESENTE NA TERAPIA PSICANALÍTICA',
+    link:'psicanalise-40.php'
+},
+{
+    img:'coracao.jpg',
+    titulo:'PSICANÁLISE: AJUDA PARA ENFRENTAR A DIFICULDADE DE INTERPRETAR NOSSOS SENTIMENTOS E EMOÇÕES',
+    link:'psicanalise-41.php'
+},
+{
+    img:'destino.jpg',
+    titulo:'PSICANÁLISE PARA MUDANÇA NA “PREDESTINAÇÃO”',
+    link:'psicanalise-42.php'
+},
+{
+    img:'liberdade.jpg',
+    titulo:'ANÁLISE: CAMINHO PARA LIVRAR-SE DE SINTOMAS INCÔMODOS',
+    link:'psicanalise-43.php'
+},
+{
+    img:'libertar.jpg',
+    titulo:'RAZÃO PARA FAZER ANÁLISE: SAIR DE UMA CONDIÇÃO DIFÍCIL',
+    link:'psicanalise-44.php'
+},
+{
+    img:'lampada.jpg',
+    titulo:'RAZÕES PARA FAZER TERAPIA 3',
+    link:'psicanalise-45.php'
+},
+{
+    img:'biblioteca.jpg',
+    titulo:'RAZÕES PARA FAZER TERAPIA EM PSICANÁLISE 2:',
+    link:'psicanalise-46.php'
+},
+{
+    img:'conversa.jpg',
+    titulo:'RAZÕES PARA FAZER TERAPIA, SEGUNDO JUNG',
+    link:'psicanalise-47.php'
+},
+{
+    img:'psicoterapia',
+    titulo:'A PRÁTICA DA PSICOTERAPIA E O CAMINHO DA CURA',
+    link:'psicanalise-48.php'
+},
+{
+    img:'terapia.jpg',
+    titulo:'POR QUE FAZER TERAPIA COM PSICANÁLISE?',
+    link:'psicanalise-49.php'
+}
+]
+
+/*var artigoFinal = []
+for(let i=0;i<artigo.length;i++){
+    artigoFinal[i] = new Artigo(artigo[i].img,titulos[i],artigo[i].link)
+}
+console.log(artigoFinal[1].titulo)*/
+
+export default Artigo;
