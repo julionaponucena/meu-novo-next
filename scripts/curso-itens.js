c8ee9868a51adf15ce3653f50e44394c2c616491
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-05 22:59:02 -0300

    Create curso-itens.js

---
+++ scripts/curso-itens.js
@@ -0,0 +1,53 @@
+const artigoCursos = [{
+    img:'psicologia-fundo.jpg',
+    titulo:'CONTRIBUIÇÃO DA PSICANÁLISE PARA A EDUCAÇÃO EMOCIONAL DOS EDUCADORES',
+    link:'curso-1.php'
+},
+{
+    img:'puzzle.jpg',
+    titulo:'FILOSOFIA PARA CRIANÇAS NA PRÁTICA ESCOLAR',
+    link:'curso-2.php'
+},
+{
+    img:'pai-e-filho.jpg',
+    titulo:'GRUPO DE PAIS: DESENVOLVENDO VALORES NA EDUCAÇÃO',
+    link:'curso-3.php'
+},
+{
+    img:'educacao2.jpg',
+    titulo:'DESENVOLVENDO VALORES NA EDUCAÇÃO',
+    link:'curso-4.php'
+},
+{
+    img:'maos-dadas.jpg',
+    titulo:'RELAÇÕES HUMANAS NO AMBIENTE DE TRABALHO',
+    link:'curso-5.php'
+},
+{
+    img:'aperto-maos.jpg',
+    titulo:'A EVOLUÇÃO NA GESTÃO DE PESSOAS',
+    link:'curso-6.php'
+},
+{
+    img:'etica.jpg',
+    titulo:'ÉTICA PROFISSIONAL E ADMINISTRAÇÃO DO TEMPO',
+    link:'curso-7.php'
+},
+{
+    img:'mulher.jpg',
+    titulo:'Autoconhecimento através da História da Filosofia',
+    link:'curso-8.php'
+},
+{
+    img:'book.jpg',
+    titulo:'Entendendo a educação à luz da Filosofia Clínica',
+    link:'curso-9.php'
+},
+{
+    img:'atenas.jpg',
+    titulo:'O exercício de filosofar e seus efeitos em nossa vida',
+    link:'curso-10.php'
+}
+]
+
+export default artigoCursos
