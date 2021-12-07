7ea06f8b4bde6ed9e319b52dee7b939c695c15af
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-06 21:16:36 -0300

    Update psicanalise-menu.js

--- pages/psicanalise-menu.js
+++ pages/psicanalise-menu.js
@@ -1,16 +1,17 @@
 import Header from "../include/header";
 import Menu from "../include/menu";
 import Footer from "../include/footer";
-import Artigo from "../scripts/menu-itens";
+import Artigo from '../menu/menu-itens';
 import HiperLink from "../scripts/hiperlinks";
+import ContainerMenu from "../menu/container-menu";
 function PsicanaliseMenu (){
     return(
         <>
             <Header/>
             <Menu/>
-            <section className="hiper-menu">
-                <HiperLink obj={Artigo}/>
-            </section>
+            <ContainerMenu titulo="Psicanalise">
+                <HiperLink artigo={Artigo}/>
+            </ContainerMenu>
             <Footer/>
         </>
     )
