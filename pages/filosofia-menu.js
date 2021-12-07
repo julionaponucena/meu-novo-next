541017f4cec8030be3d44222db17d751f0a400e0
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-07 15:22:03 -0300

    Update filosofia-menu.js

--- pages/filosofia-menu.js
+++ pages/filosofia-menu.js
@@ -4,9 +4,11 @@ import Footer from "../include/footer"
 import ArtigoFilosofia from "../scripts/filosofia-itens"
 import HiperLink from "../scripts/hiperlinks"
 import ContainerMenu from "../menu/container-menu"
+import HeadConfig from "../include/head"
 function FilosofiaMenu (){
     return(
         <>
+            <HeadConfig/>
             <Header/>
             <Menu/>
             <ContainerMenu titulo="Filosofia">
