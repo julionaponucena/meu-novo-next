f7d2361ac91fe17e1de011a2e0cb970df2d30a5c
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-07 15:22:09 -0300

    Create filosofia-1.js

---
+++ pages/filosofia/filosofia-1.js
@@ -0,0 +1,25 @@
+import Header from "../../include/header";
+import Principal from "../../supertags/principal";
+import Menu from "../../include/menu";
+import Footer from "../../include/footer";
+import Titulo from "../../scripts/titulo";
+import HeadConfig from "../../include/head";
+
+function Filosofia_01 (){
+    return (
+        <>
+            <HeadConfig/>
+            <Header/>
+            <Menu/>
+            <Principal>
+                <Titulo>TIMIDEZ: CAUSAS E FORMAS DE SUPERAÇÃO</Titulo>
+                <p>A reflexão a seguir tem o objetivo de responder a um questionamento feito por Rafael Pazzini, a quem agradeço pela colaboração. Embora o desenvolvimento da timidez possa também ser associado a aspectos biológicos, as causas predominantes para o seu desenvolvimento estão no campo da interação social que marca a trajetória do indivíduo desde a sua infância. O nível de aceitação e fluidez dentro dos grupos sociais, como os colegas de escola, os amigos da vizinhança e os tipos de estímulo ou críticas sofridas nas diversas  fases do seu desenvolvimento são elementos capazes de conduzir o indivíduo a formar uma visão de potência ou impotência a respeito de si mesmo. Quando ele se sente aceito e recebe reforços positivos às suas ações, a criança entende que aquilo que ela está fazendo é bom e desenvolve acerca de si mesmo uma imagem positiva. Ao contrário, quando ela é desestimulada diante de uma ação mal sucedida, existe a tendência do desenvolvimento de uma crença de incapacidade acerca de si mesmo, dando início a um ciclo de insegurança, medo da exposição diante da formação de uma visão prévia do fracasso e a consequente repressão. Pais autoritários ou superprotetores contribuem para formar filhos tímidos por criar barreiras para que eles enfrentem situações cotidianas que possam encorajá-los a arriscar-se mais e ficar menos receosos à tomada de iniciativa.</p>
+
+                <p>A superação da timidez certamente auxilia na formação de  uma imagem mais positiva acerca de si mesmo, sendo essencial para o desenvolvimento de uma autoestima saudável. O processo terapêutico pode ajudar a pessoa a lidar com essa situação, na medida em que, através da investigação aprofundada sobre si que esse processo propõe, ela adquire as condições de ressignificar seus sentimentos e emoções diante das mais diversas situações que possam impedi-lo de   realizar-se plenamente. Com o trabalho desenvolvido na terapia a pessoa pode aprender habilidades sociais, como ter consciência de sua timidez e maneiras de entender quando ela resulta em um pensamento irracional. Quer saber como você pode melhorar suas habilidades sociais e melhorar suas relações inter e intra pessoais? Entre em contato e agende uma avaliação.</p>
+            </Principal>
+            <Footer/>
+        </>
+    )
+}
+
+export default Filosofia_01;
