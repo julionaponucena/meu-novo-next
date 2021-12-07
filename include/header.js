4b52a9b1c20a61e0bc8f26498761d619fb2d4a4d
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-07 15:21:40 -0300

    Update header.js

--- include/header.js
+++ include/header.js
@@ -6,14 +6,14 @@ import Image from "next/image";
 function Header (){
     
     return (<header>
-        <div class="cabecalho"> 
-                <div class="logo-box">
-                    <Image class= "logo" src="/logo-mente.png" width={1000} height={1000} />
+        <div className="cabecalho"> 
+                <div className="logo-box">
+                    <Image className= "logo" src="/logo-mente.png" width={1000} height={1000} />
                 </div>
                 <h1> Humanamente</h1>
                
             </div>
-            <div class="redes-sociais inside-header">
+            <div className="redes-sociais inside-header">
                 <a target="_blank">
 
                     
