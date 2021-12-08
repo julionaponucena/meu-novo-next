cc0f0245a44d23b4af1ab36f21bf76265a95db69
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-07 21:08:08 -0300

    Update footer.js

--- include/footer.js
+++ include/footer.js
@@ -8,15 +8,15 @@ function Footer (){
         <><footer>
             <div className="conteudo-rodape">
                 <p>Siga-nos também nas redes sociais</p>
-                <a target="_blank" href="https://www.instagram.com/psicanalistaaparecidoalcantara/"><Image src={Instagram} width={25} height={25}alt="Instagram" />
+                <a target="_blank" href="https://www.instagram.com/psicanalistaaparecidoalcantara/">
                 </a>
                 <a target="_blank" href="https://www.facebook.com/isaltosp">
-                    <Image src={Facebook} width={25} height={25} />
+                   
                 </a>
             </div>
         </footer><a className="terapia" target="_blank" href="https://api.whatsapp.com/send?phone=5511971252865">
                 <span> Agende sua sessão online</span>
-            </a><a target="_blank" className="whatsapp" href="https://api.whatsapp.com/send?phone=5511971252865"> <Image src={Whatsapp} width={70}height={70}/></a></>
+            </a><a target="_blank" className="whatsapp" href="https://api.whatsapp.com/send?phone=5511971252865"> </a></>
     
     )
 }
