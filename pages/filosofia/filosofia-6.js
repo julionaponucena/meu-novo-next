449a9b3208ecb522d8d1dcfedd862b07151f434b
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-09 19:35:37 -0300

    Create filosofia-6.js

---
+++ pages/filosofia/filosofia-6.js
@@ -0,0 +1,26 @@
+import HeadConfig from "../../include/head";
+import Header from "../../include/header";
+import Menu from "../../include/menu";
+import Footer from "../../include/footer";
+import Titulo from "../../supertags/titulo";
+import Principal from "../../supertags/principal";
+
+function Filosofia_06 (){
+    return(
+        <>
+        <HeadConfig/>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>CULTIVAR A ESPERANÇA PODE TE FORTALECER  DIANTE DAS DIFICULDADES</Titulo>
+            <p>Há um velho ditado popular que diz que a esperança é a última que morre. Embora nem sempre conseguimos conferir realidade a esse ditado, é muito importante investir no desafio de manter viva a esperança, sobretudo porque é através dela que damos sentidos reais à nossa existência, visto que ela é capaz de nos proporcionar a necessária força de reação diante de cenários que nos são desfavoráveis.</p>
+            <p>Podemos constatar o efeito da esperança na vida das pessoas que a cultivam pelas respostas  positivas e funcionais que elas são capazes de oferecer em momentos de exposição às mais variadas dificuldades enfrentadas por elas. Observamos que essas pessoas não se deixam abater facilmente, estão sempre em busca de uma saída, mesmo que ela aparentemente não exista. Além disso elas possuem o poder de contagiar aqueles que estão à sua volta, mobilizando-os a não desistir e não se desesperar. O termo desesperar é tratado aqui como o ato desprover-se de esperança. Em outras palavras, quando deixamos de nutrir a esperança, criamos um espaço que será preenchido por outros sentimentos, os quais podem potencializar atitudes destrutivas e disfuncionais, como aquelas tomadas em situação de desespero.</p>
+            <p>Do ponto de vista psíquico, estudos revelam que pessoas que costumam cultivar a esperança possuem maior índice de desempenho nos estudos, no campo profissional e nos demais setores da vida, porque a postura positiva  pode produzir como resultado uma outra característica desejável a todos nós que é a resiliência. A resiliência consiste na capacidade que o indivíduo possui para lidar com situações adversas, de adaptar-se a situações novas, procurar saídas efetivas para os obstáculos, além de saber controlar-se diante de situações de pressão e estresse. Considerando que a vida não se faz somente de bons acontecimentos, podemos afirmar que a pessoa resiliente tem grandes possibilidades de conferir melhores condições à sua vida sob os mais variados aspectos.</p>
+            <p>Se você deseja fazer uma autoanálise a respeito do assunto, sugiro que comece por entender a diferença ente cultivar a esperança e a mera expectativa passiva de que aconteça aquilo que deseja. A esperança a que me refiro é dotada de comprometimento por parte do indivíduo diante das mudanças que ele deseja ver materializadas, exigindo por parte dele um esforço que o mobilize a dar vida àquilo que quer para si, acreditando na possibilidade de realização. Nessa perspectiva a esperança pressupõe um exercício de fé, de convicção na possibilidade de realização, mesmo que aparentemente a situação verificada no presente sugira outro resultado. Em situações em que as coisas não estejam  acontecendo da forma como gostaríamos, temos duas alternativas: a primeira é dar ouvidos à desesperança e adotar medidas de destempero, metendo os pés pelas mãos, desistindo de buscar alternativas, submetendo-se àquilo que parece ser o nosso destino. A segunda alternativa passa pela adoção de uma atitude mental positiva, com o intuito de elevar nosso nível de esforço visando encontrar caminhos que nos levem à superação e ao alcance dos nossos objetivos. Essa atitude nos permite assumir o controle da situação, mesmo diante de condições adversas, nos permitindo inclusive, agir com a sabedoria que nos permite mudar o direcionamento das nossas metas, se considerarmos que elas já não fazem mais sentido ou que as novas condições já não permitem o seu desenrolar.</p>
+            <p>O caminho para alcançar tais mudanças passa por reflexão, realização de uma consulta íntima, procurando sempre manter o pensamento positivo, aliado a atitudes que elevem sua espiritualidade e o conduzam a autovalorização, sentimento próprio de quem deseja investir cada vez mais em sua realização e busca pela verdadeira felicidade. Se você se sente fragilizado e impotente para operacionalizar tais mudanças por si só, procure a ajuda de um profissional devidamente capacitado para realizar contigo essa jornada em busca de si mesmo e do sentido da sua existência.</p>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+export default Filosofia_06
