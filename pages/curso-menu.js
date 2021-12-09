df0046da947181943bddff1d6585d370855ca141
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-09 16:42:52 -0300

    Update curso-menu.js

--- pages/curso-menu.js
+++ pages/curso-menu.js
@@ -1,8 +1,8 @@
-
+import HiperLink from "../scripts/hiperlinks"
 import Header from "../include/header"
 import Menu from "../include/menu"
 import Footer from "../include/footer"
-
+import artigoCursos from "../menu/curso-itens"
 import ContainerMenu from "../menu/container-menu";
 import HeadConfig from "../include/head";
 function CursoMenu (){
@@ -12,7 +12,7 @@ function CursoMenu (){
             <Header/>
             <Menu/>
             <ContainerMenu titulo="CURSOS">
-               
+               <HiperLink artigo={artigoCursos}/>
             </ContainerMenu>
             <Footer/>
         </>
