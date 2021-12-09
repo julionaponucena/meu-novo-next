1f255ed375b9432637114cd5362526bdda97b3f1
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-09 19:35:31 -0300

    Create filosofia-5.js

---
+++ pages/filosofia/filosofia-5.js
@@ -0,0 +1,21 @@
+import HeadConfig from "../../include/head";
+import Header from "../../include/header";
+import Menu from "../../include/menu";
+import Titulo from "../../supertags/titulo";
+import Principal from "../../supertags/principal";
+import Footer from "../../include/footer";
+function Filosofia_05 (){
+  return(   <>
+    <HeadConfig/>
+    <Header/>
+    <Menu/>
+    <Principal>
+        <Titulo>CONSELHOS DE SÓCRATES PARA PLENIFICAR NOSSA EXISTÊNCIA</Titulo>
+        <p>Na Grécia Antiga, o grande pensador Sócrates, de forma muito simples, sem ser simplória, costumava inquirir seus interlocutores acerca de questões complexas e de suma importância para a condução da  existência humana. Sua preocupação era conduzir as pessoas às veredas das reflexões sobre a natureza e o sentido da vida, objetivando contribuir para o entendimento de atributos importantes como virtudes, vícios, bem, mal, riqueza, pobreza, etc. Tudo isso com o intuito de ajudar na formação do homem em sua integralidade. Ao fazer uso das indagações (maiêutica), como era sua prática,  o filósofo buscava colocar o interlocutor diante do desafio de buscar a essência daquilo que cultuam como verdade, afim de alcançar o entendimento sobre os pressupostos que alicerçam sua vida. Para isso ele lançou mão de uma arma poderosa que é a arte de desenvolver perguntas pertinentes e profundas com a intenção de se atingir o cerne das questões, por mais simples que elas pudessem parecer.</p>
+        <p>A sociedade atual, marcada pelo imediatismo e pela superficialidade, nos impele a retornar aos ensinamentos do velho mestre, sobretudo porque nos encontramos diante do grande desafio de conferir um significado à nossa existência.  Nesse retorno convém citar o exemplo que Sócrates utiliza, tendo como referência a profissão do seu pai Sofronisco, o qual exercia o ofício de escultor. Em sua analogia entre o  ofício desenvolvido por seu pai e aquilo que a existência  espera de cada um de nós, Sócrates afirma que antes mesmo de começar a trabalhar na pedra bruta, o escultor já possui uma ideia da peça que ele irá construir. Isso se torna possível porque a habilidade artística do escultor, associada à sua sensibilidade, o capacita a ver o que está além da aparência bruta da matéria em si, vislumbrando nela aquilo que ela possui como potencialidade. Da mesma forma,  cada ser humano nasce repleto de potencialidades, sendo que sua principal função existencial é atuar de forma reflexiva,  afim de atuar no sentido de possibilitar  todas as condições para que elas se plenifiquem.</p>
+    </Principal>
+    <Footer/>
+    </>)
+}
+
+export default Filosofia_05
