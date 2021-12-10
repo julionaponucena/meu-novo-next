bc84a9081d0a663e11d0854a6d9b1a7268c18a2d
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-10 16:44:56 -0300

    Create psicanalise-37.js

---
+++ pages/psicanalise/psicanalise-37.js
@@ -0,0 +1,22 @@
+import HeadConfig from "../../include/head"
+import Header from "../../include/header"
+import Menu from "../../include/menu"
+import Footer from "../../include/footer"
+import Principal from "../../supertags/principal"
+import Titulo from "../../supertags/titulo"
+
+function Psicanalise_37 (){
+    return (
+        <>
+        <HeadConfig/>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>COMO LIDAR COM O SENTIMENTO DA RAIVA?</Titulo>
+            <p>Um dos temas recorrentes levados pelos pacientes à análise é a incapacidade de entender e lidar com o sentimento da raiva. Sente-se raiva por algo que saiu do controle, por algo que alguém disse e que não gostamos, por um projeto que não saiu como o planejado, por estar em situação de confinamento como agora, enfim, por tudo aquilo que possa canalizar alguma frustração que experimentamos. Embora exista uma forma peculiar de manifestação que é própria de cada indivíduo, podemos afirmar que tal sentimento possui uma única raiz que é a incapacidade do indivíduo de lidar com suas próprias frustrações. Sentimos raiva porque as metas estabelecidas por nós, por algum motivo não foram atendidas. Como não estávamos devidamente preparados para a possibilidade de que aquilo que almejamos não saísse conforme esperávamos, nos entregamos a esse sentimento e aos seus desdobramentos. Assim, a principal forma de não sermos dominados por esse sentimento é começar a ponderar mais sobre as diversas possibilidades que existem a cada empreendimento que vamos desenvolver, quer seja na área profissional, acadêmica, afetiva, ou qualquer outra área. A sabedoria nos orienta a ficar atentos às nossas expectativas, conferindo a elas a devida dose de otimismo sem, no entanto, cair na ideia simplista de que basta que acreditemos que tudo sairá conforme esperamos. Quer saber um pouco mais sobre o assunto e como a psicanálise pode te ajudar a lidar com suas emoções? Entre em contato conosco e agende uma avaliação gratuita.</p>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+export default Psicanalise_37
