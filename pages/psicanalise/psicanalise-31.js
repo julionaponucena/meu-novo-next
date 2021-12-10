e27f2b770de150f4c35fef0e09dd8a0c064b0a63
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-10 16:44:39 -0300

    Create psicanalise-31.js

---
+++ pages/psicanalise/psicanalise-31.js
@@ -0,0 +1,24 @@
+import HeadConfig from "../../include/head"
+import Header from "../../include/header"
+import Menu from "../../include/menu"
+import Footer from "../../include/footer"
+import Principal from "../../supertags/principal"
+import Titulo from "../../supertags/titulo"
+
+
+function Psicanalise_31 (){
+    return (
+        <>
+        <HeadConfig/>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>O QUE EXISTE DE OBJETIVO NOS SENTIMENTOS DE INFERIORIDADE?</Titulo>
+            <p>Existem muitas pessoas que desenvolvem uma imagem negativa de si mesmo, levando uma vida como se não tivessem nenhum valor ou, no máximo, que possuem um valor muito limitado. Essa visão normalmente é construída com base na subjetividade da relação que o indivíduo estabelece com os outros à sua volta, situação em que ele fica ligado àquilo que pensam ou que falam dele, desenvolvendo assim uma relação de dependência, de aceitação e reforço externo à suas atitudes.</p>
+            <p>Considerando que os dados que colhemos como resultado do julgamento que nos fazem são dados subjetivos, por serem frutos de expectativas e medidas alheias, um dos objetivos da terapia em psicanálise e ajudar a pessoa a desenvolver um olhar mais atento àquilo que lhe é transmitido como sinais de aprovação ou de reprovação, com o intuito de que ele desenvolva a capacidade de se posicionar de maneira mais segura e positiva diante da sua própria autoimagem. Se não podemos alterar  a visão que desenvolvem  a nosso respeito, seguramente podemos  atuar sobre o seu impacto sobre a nossa vida. O primeiro passo é começarmos a procurar os dados objetivos que permitiram a nossa construção até o presente momento, procurando entender ainda quais são meus principais pontos fortes e aqueles que nos deixam vulneráveis diante da vida, decidindo-se sobre um investimento efetivo na alteração daquilo que nossa consciência nos aponta como passíveis de mudanças. Quer conhecer esse caminho de busca de si mesmo e de sua reelaboração existencial? Agende uma avaliação conosco.</p>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+export default Psicanalise_31
