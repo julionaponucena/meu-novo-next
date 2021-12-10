42d13e1a7f28e09ba3ecd25628affa28d7d88f06
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-10 16:44:58 -0300

    Create psicanalise-38.js

---
+++ pages/psicanalise/psicanalise-38.js
@@ -0,0 +1,22 @@
+import HeadConfig from "../../include/head"
+import Header from "../../include/header"
+import Menu from "../../include/menu"
+import Footer from "../../include/footer"
+import Principal from "../../supertags/principal"
+import Titulo from "../../supertags/titulo"
+
+function Psicanalise_38 (){
+    return (
+        <>
+        <HeadConfig/>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>É POSSÍVEL VIVER SEM SER CONTROLADO PELO PASSADO?</Titulo>
+            <p>nvariavelmente nossa condição existencial nos conduz a revisitar nosso passado, normalmente com o intuito de escavar a profundidade do nosso inconsciente para, quem sabe, compreender o que está acontecendo conosco em situações presentes. O trabalho psicanalítico, embora reconheça a importância de eventos passados na constituição do indivíduo que se apresenta diante do analista, considera também que o peso não está no acontecimento em si, mas no sentido que lhe fora atribuído por quem o vivenciou e, sobretudo, a razão pela qual determinados eventos se fazem presentes em sua vida até o presente. Diante de eventos assim, percebemos que em muitas ocasiões o indivíduo se apega a esses acontecimentos como justificativa para não efetuar as mudanças que ele precisa fazer em sua constituição psíquica e existencial, as quais exigem dele uma certa dose de esforço, que nem sempre ele está disposto a empreender. Por entendermos que se o indivíduo se fixa no passado ele tende a permanecer no passado, o que em nada contribui para sua plena realização presente e futura, nosso papel é contribuir para que o paciente em tratamento psicanalítico se desvincule dessa visão fatalista que une passado e presente como algo já determinado, conduzindo-o a novas possibilidades de representação de si mesmo e da sua história, contribuindo assim para que ele avance em direção a realização de metas que possam conduzi-lo à sua realização pessoal. Quer saber mais sobre a atuação da psicanálise e como ela pode contribuir contigo? Entre em contato conosco e agende uma sessão.</p>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+export default Psicanalise_38
