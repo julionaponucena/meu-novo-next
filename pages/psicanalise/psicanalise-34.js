f538d0d7fb035d3b1c1f54d592163a9acc0e29d7
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-10 16:44:48 -0300

    Create psicanalise-34.js

---
+++ pages/psicanalise/psicanalise-34.js
@@ -0,0 +1,22 @@
+import HeadConfig from "../../include/head"
+import Header from "../../include/header"
+import Menu from "../../include/menu"
+import Footer from "../../include/footer"
+import Principal from "../../supertags/principal"
+import Titulo from "../../supertags/titulo"
+
+function Psicanalise_34 (){
+    return (
+        <>
+        <HeadConfig/>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>POR QUE MUITOS OPTAM POR NÃO MUDAR?</Titulo>
+            <p>Você já reparou que apesar do sofrimento que os atormenta muitas pessoas insistem em manter um determinado padrão de comportamento e estilo de vida? Ao analisarmos as justificativas oferecidas para a ausência de atitudes que indiquem efetivamente o desejo de mudança, verificamos que as pessoas geralmente apresentam determinadas tendências de comportamento e ações que estão muito ligadas à forma como ela se construiu ao longo da vida, construção esta que, embora não ofereça o resultado almejado, representa o melhor que ele pode ser, por se tratar do seu porto seguro. O estilo de vida de uma pessoa reflete como ela enxerga o mundo e como ela enxerga e si própria. Esse estilo não é imutável e já determinado pelo nosso passado como muitos pensam. O problema é que para mudá-lo, de forma que a vida possa ser mais feliz e realizada, exige um certo esforço que nem todos estão dispostos a fazer. Daí a opção por se manter da forma como está, mesmo que isso possa provocar sofrimento. Quando nos decidimos por mudar colocamos nossa coragem e capacidade de perseverança em prova. Por isso ousamos afirmar que o propósito de mudança é um grande demonstrativo da coragem que o indivíduo carrega dentro de si. Você se interessou pelo assunto e gostaria de obter auxílio para deixar o seu porto seguro e partir para uma viagem que lhe permita ampliar seus horizontes existenciais? Entre em contato conosco e agende uma consulta.</p>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+export default Psicanalise_34
