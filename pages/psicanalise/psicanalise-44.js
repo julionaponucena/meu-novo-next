fc9f908e29c8d598d0eab964a6ffd5bfbf906e43
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-10 18:03:10 -0300

    Create psicanalise-44.js

---
+++ pages/psicanalise/psicanalise-44.js
@@ -0,0 +1,22 @@
+import HeadConfig from "../../include/head"
+import Header from "../../include/header"
+import Menu from "../../include/menu"
+import Footer from "../../include/footer"
+import Principal from "../../supertags/principal"
+import Titulo from "../../supertags/titulo"
+
+function Psicanalise_44 () {
+    return (
+        <>
+        <HeadConfig/>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>RAZÃO PARA FAZER ANÁLISE: SAIR DE UMA CONDIÇÃO DIFÍCIL</Titulo>
+            <p>A maioria das pessoas procura o consultório porque está em um momento complicado. Podemos citar aqui muitas situações que causam sofrimento como o término de um relacionamento, o falecimento de alguém querido, mudança de cidade ou país, ou qualquer sofrimento psíquico que seja tão grande que provoca a paralisação do indivíduo. Nessas situações a ajuda de alguém preparado para ouvir, fazer o acolhimento das queixas com o intuito de auxiliar no encontro de alternativas que possibilitem ao paciente a ressignificação acerca das representações formadas diante daquilo que está lhe sucedendo é de fundamental importância. Quer conhecer como funciona o atendimento com psicanálise? Entre em contato conosco e agende uma sessão.</p>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+export default Psicanalise_44
