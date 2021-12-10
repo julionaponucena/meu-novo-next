ce4ea4d4ce15216d40644943ebe47b97288c563a
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-10 16:45:05 -0300

    Create psicanalise-40.js

---
+++ pages/psicanalise/psicanalise-40.js
@@ -0,0 +1,22 @@
+import HeadConfig from "../../include/head"
+import Header from "../../include/header"
+import Menu from "../../include/menu"
+import Footer from "../../include/footer"
+import Principal from "../../supertags/principal"
+import Titulo from "../../supertags/titulo"
+
+function Psicanalise_40 () {
+    return (
+        <>
+        <HeadConfig/>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>RELAÇÃO ENTRE PASSADO E PRESENTE NA TERAPIA PSICANALÍTICA</Titulo>
+            <p>Embora a psicanálise se ampare na história do indivíduo com o intuito de ajudá-lo a compreender sua própria trajetória existencial e as possíveis razões que o formaram psiquicamente, ela tem uma visão para o momento presente com uma perspectiva para aquilo que o indivíduo pretende construir para a sua trajetória presente e futura. Assim, podemos afirmar que ela se ampara na etiologia (estudo das causas de determinados sintomas e sofrimentos e a própria formação psíquica) para o desenvolvimento mais apropriado da teleologia (que é o estudo das metas existenciais ostentada pelo indivíduo, com base nas suas perspectivas presentes e futuras). Sem levarmos isso em consideração, o trabalho analítico corre o risco de ficar estéril, na medida em que não contribuirá para  o nascimento de um outro ser, que na dinâmica existencial pretendemos gerar em nós a partir das nossas experiências, normalmente permeadas por dores e delícias, vitórias e fracassos. O trabalho do analista junto ao seu partilhante é o de ajudá-lo a acessar os pontos de conflito psíquico que de certa forma estão dificultando o desenvolvimento de tais perspectivas, tomando emprestado o pensamento do filósofo existencialista francês Jean Paul Sartre: "O importante não é o que fazemos de nós, mas o que nós fazemos daquilo que fazem de nós." Quer conhecer melhor como a psicanálise pode te ajudar nessa busca? Entre em contato conosco.</p>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+export default Psicanalise_40
