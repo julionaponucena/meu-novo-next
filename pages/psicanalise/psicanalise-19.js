b2b03991fe02fd805e58d40b6c33c9ed50a98c38
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-10 13:27:43 -0300

    Create psicanalise-19.js

---
+++ pages/psicanalise/psicanalise-19.js
@@ -0,0 +1,24 @@
+import HeadConfig from "../../include/head"
+import Header from "../../include/header"
+import Menu from "../../include/menu"
+import Footer from "../../include/footer"
+import Principal from "../../supertags/principal"
+import Titulo from "../../supertags/titulo"
+
+function Psicanalise_19 (){
+    return (
+        <>
+        <HeadConfig/>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>LIVRAR-SE DA NECESSIDADE DE RECONHECIMENTO PODE SER LIBERTADOR </Titulo>
+            <p>Um dos grandes agentes sabotadores da  liberdade humana é a necessidade de reconhecimento que muitas pessoas possuem. Isso acontece porque, na medida em que a pessoa cultiva a necessidade de que as demais pessoas a reconheçam a partir daquilo que ela representa ser ou faz, suas atitudes e sua própria constituição existencial ocorrerão em função de atender às expectativas criadas por essas pessoas. Com isso ela deixa de agir de acordo com seus princípios ou suas próprias vontades, orientando-se em direção àquilo que o mundo exterior sinaliza esperar dela. Quando isso acontece, a pessoa se conduz a uma situação de aprisionamento tal, de sorte que, com o passar do tempo ela deixa de se reconhecer como a protagonista da sua própria história, imersa que está na situação de alienação de si que ela mesma se permitiu entrar.</p>
+
+            <p>O caminho para se libertar dessa necessidade passa pelo reconhecimento das próprias qualidades, independentemente de serem ou não percebidas pelos outros, ocasião em que a pessoa se assenhora de si a partir daquilo que ela pode lhe oferecer, bem como àqueles que a cercam. Assim ela começa a andar por caminhos mais seguros alcançando mais satisfação diante da vida, amparando-se em suas próprias conquistas. O ponto de partida normalmente passa pela tomada de consciência de si  e das buscas por aquilo que realmente contribuem para o engrandecimento e fortalecimento do indivíduo como um todo.</p>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+export default Psicanalise_19
