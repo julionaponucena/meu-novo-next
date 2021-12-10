bcdf1070f8ed704df4eabb03a4494f095e22c2bc
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-10 18:03:19 -0300

    Create psicanalise-47.js

---
+++ pages/psicanalise/psicanalise-47.js
@@ -0,0 +1,34 @@
+import HeadConfig from "../../include/head"
+import Header from "../../include/header"
+import Menu from "../../include/menu"
+import Footer from "../../include/footer"
+import Principal from "../../supertags/principal"
+import Titulo from "../../supertags/titulo"
+
+function Psicanalise_47 (){
+    return (
+        <>
+        <HeadConfig/>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>RAZÕES PARA FAZER TERAPIA, SEGUNDO JUNG</Titulo>
+            <p>Jung, em seu livro “A Psicologia e a Alquimia” (no qual ele analisa mais de 800 sonhos de um paciente de sua clínica), faz a seguinte afirmação sobre o tratamento psicoterapêutico:</p>
+            <p>No processo analítico, isto é, no confronto dialético do consciente e do inconsciente constata-se um desenvolvimento, um progresso em direção a uma certa meta ou fim cuja natureza enigmática me ocupou durante anos a fio. Os tratamentos psíquicos podem chegar a um fim em todos os estágios possíveis do desenvolvimento, sem que por isso se tenha o sentimento de ter alcançado uma meta. Certas soluções típicas e temporárias ocorrem:</p>
+            <ol className="lista-conteudo">
+                <li>depois que o indivíduo recebeu um bom conselho;</li>
+                <li>depois de uma confissão mais ou menos completa;</li>
+                <li>depois de haver reconhecido um conteúdo essencial, até então inconsciente, cuja conscientização imprime um novo impulso à sua vida e às suas atividades;</li>
+                <li>depois de libertar-se da psique infantil após um longo trabalho efetuado;</li>
+                <li>depois de conseguir uma nova adaptação racional a condições de vida talvez difíceis ou incomuns;</li>
+                <li>depois do desaparecimento de sintomas dolorosos;</li>
+                <li>depois de uma mudança positiva do destino, tais como exames, noivado, casamento, divórcio, mudança de profissão, etc;</li>
+                <li>depois da redescoberta de pertencer a uma crença religiosa ou de uma conversão;</li>
+                <li>depois de começar a erigir uma filosofia de vida (“filosofia”, no antigo sentido da palavra”). Se bem que a esta enumeração possam ser introduzidas diversas modificações, ela define de um modo geral as principais situações em que o processo analítico ou psicoterapêutico chega a um fim provisório, ou às vezes definitivo (JUNG, p. 18).</li>
+            </ol>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+export default Psicanalise_47
