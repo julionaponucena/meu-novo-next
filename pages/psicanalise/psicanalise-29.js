42e2c91d40f32ead947e91bc53b6d08dd3254a17
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-10 13:28:15 -0300

    Create psicanalise-29.js

---
+++ pages/psicanalise/psicanalise-29.js
@@ -0,0 +1,24 @@
+import HeadConfig from "../../include/head"
+import Header from "../../include/header"
+import Menu from "../../include/menu"
+import Footer from "../../include/footer"
+import Principal from "../../supertags/principal"
+import Titulo from "../../supertags/titulo"
+
+function Psicanalise_29 (){
+    return (
+        <>
+        <HeadConfig/>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>USE O SENTIMENTO DE INFERIORIDADE COMO SEU ALIADO</Titulo>
+            <p>Dando segmento ao assunto abordado na postagem anterior acerca de como o sentimento de inferioridade pode atrapalhar o  desenvolvimento das nossas potencialidades e, consequentemente atuar como inibidor da nossa realização, nesta postagem desejo convidá-lo a refletir sobre o outro lado da moeda: aquele que coloca o sentimento de inferioridade como o princípio motor das nossas atitudes em busca da superação do próprio sentimento e em busca da nossa realização.</p>
+            <p>Olhando pelo aspecto da nossa formação psíquica, podemos afirmar que o indivíduo, quando chega nesse mundo, dado à sua situação de total dependência, situa-se como ser totalmente impotente. Ao mesmo tempo, ele possui o desejo universal de escapar desse estado de impotência, lançando-se em  busca por aquilo que Alfred Adler chama de “busca de superioridade”, ou seja, a busca por um estado ideal. Nessa situação, se usado de forma saudável, o sentimento de inferioridade é capaz de promover no indivíduo o combustível que alimentará esse desejo de crescimento, uma vez que a sua condição atual lhe provoca desconforto e sofrimento. Dessa maneira, a pessoa parte para um processo de superação das próprias fragilidades que ele detectou em si, tornando-se o construtor de si mesmo, a exemplo do artesão que se põe a moldar o barro disforme, dando-lhe os contornos que ele deseja para si mesmo. Quando não existe essa busca, é muito comum a pessoa se entregar à situação que o incomoda, fazendo com que aquilo que era um sentimento de inferioridade torne-se, com o desenrolar da sua própria história em um complexo de inferioridade, resultando em muitas outras dificuldades existenciais.</p>
+            <p>Como você tem lidado com seus sentimentos e emoções? Sente-se capaz de reconhecê-los a ponto de conferir-lhes papeis funcionais em sua existência? Que tal conversarmos a respeito disso? Agende sua avaliação.</p>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+export default Psicanalise_29
