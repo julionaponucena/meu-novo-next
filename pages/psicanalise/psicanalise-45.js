51d76b3e31f21f529516b72989afdc4ff6cc6609
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-10 18:03:13 -0300

    Create psicanalise-45.js

---
+++ pages/psicanalise/psicanalise-45.js
@@ -0,0 +1,24 @@
+import HeadConfig from "../../include/head"
+import Header from "../../include/header"
+import Menu from "../../include/menu"
+import Footer from "../../include/footer"
+import Principal from "../../supertags/principal"
+import Titulo from "../../supertags/titulo"
+
+function Psicanaliise_45 (){
+    return (
+        <>
+        <HeadConfig/>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>RAZÕES PARA FAZER TERAPIA 3</Titulo>
+            <h2>LIBERTAR-SE DA INFÂNCIA</h2>
+            <p>Atualmente, vemos em nossa sociedade ocidental diversas pessoas que são adultas apenas na idade. Psiquicamente são tão crianças (no melhor dos casos adolescentes) que não assumem responsabilidades nem querer definir que rumo vão seguir. É neste sentido que Jung diz “libertar-se da psique infantil após um longo trabalho efetuado”, ou seja, conseguir seguir o desenvolvimento psíquico normal, que exige mais cedo ou mais tarde que rompamos a ligação simbiótica com os pais – e, mais frequentemente, com a mãe.</p>
+            <p>É muito comum vermos a diferença na maneira de lidar com o complexo familiar em uma família com 3, 4 irmãos. Alguns saem de casa rápido, casam-se logo e desligam-se da família de forma saudável e tranquila, enquanto outros nunca vão conseguir sair. A questão aqui não é que a pessoa tem que sair, mas a capacidade ou incapacidade de fazê-lo. Por exemplo, uma pessoa que quer ir fazer uma faculdade em outro estado (tem todas as condições) mas não faz por medo.</p>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+export default Psicanaliise_45
