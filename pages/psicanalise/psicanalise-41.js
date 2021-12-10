dcb269eaf7f9e3c2cbe8b3dce419138d0a2a86bf
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-10 18:03:01 -0300

    Create psicanalise-41.js

---
+++ pages/psicanalise/psicanalise-41.js
@@ -0,0 +1,22 @@
+import HeadConfig from "../../include/head"
+import Header from "../../include/header"
+import Menu from "../../include/menu"
+import Footer from "../../include/footer"
+import Principal from "../../supertags/principal"
+import Titulo from "../../supertags/titulo"
+
+function Psicanalise_41 () {
+    return (
+        <>
+        <HeadConfig/>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>PSICANÁLISE: AJUDA PARA ENFRENTAR A DIFICULDADE DE INTERPRETAR NOSSOS SENTIMENTOS E EMOÇÕES</Titulo>
+            <p>Uma das questões recorrentes que os partilhantes deixam transparecer durante a análise é a dificuldade que eles apresentam em identificar seus sentimentos e emoções diante daquilo que estão enfrentando como possível problema em sua vida. Uso aqui o termo possível problema, porque, diante da condição de se visualizar com clareza o que está lhe ocorrendo, o indivíduo pode incorrer em um erro  muito comum que é o de atribuir significados distintos aos seus eventos existenciais, situação nas quais eventos que notadamente são oportunidade para seu crescimento e fortalecimento, ganham o status de algo penoso e prejudicial. Tudo porque a pessoa teima em observar o que está acontecendo somente com o olhar da objetividade racional e fenomenológica. O trabalho do psicanalista diante de casos assim, é contribuir para que o partilhante seja conduzido ao caminho da análise subjetiva das suas impressões e representações sobre sua vida e tudo aquilo que a circunda, partindo-se do princípio de que existe em cada um de nós um oceano de elementos desconhecidos que, embora inconscientes, teimam em se manifestar constantemente. Quer saber mais sobre como funciona uma sessão de psicanálise e como ele pode te ajudar? Entre em contato conosco.</p>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+export default Psicanalise_41
