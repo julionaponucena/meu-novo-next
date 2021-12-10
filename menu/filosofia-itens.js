1e20f87c6ec9a21a1b60b1bb0d36c3f7fed36d86
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-09 21:25:54 -0300

    Update filosofia-itens.js

--- menu/filosofia-itens.js
+++ menu/filosofia-itens.js
@@ -1,67 +1,67 @@
 const artigoFilosofia = [{
     img:null,
     titulo:'TIMIDEZ: CAUSAS E FORMAS DE SUPERAÇÃO',
-    link:'filosofia-1.php'
+    link:'filosofia-1'
 },
 {
     img:null,
     titulo:'SENTIR RAIVA  SEMPRE É NEGATIVO?',
-    link:'filosofia-2.php'
+    link:'filosofia-2'
 },
 {
     img:null,
     titulo:'A ARTE DE VIVER A ALEGRIA DE SER UMA PESSOA COM DIGNIDADE',
-    link:'filosofia-3.php'
+    link:'filosofia-3'
 },
 {
     img:null,
     titulo:'A PRÁTICA DA PSICOTERAPIA E O CAMINHO DA CURA',
-    link:'filosofia-4.php'
+    link:'filosofia-4'
 },
 {
     img:'yoga.jpg',
     titulo:'CONSELHOS DE SÓCRATES PARA PLENIFICAR NOSSA EXISTÊNCIA',
-    link:'filosofia-5.php'
+    link:'filosofia-5'
 },
 {
     img:null,
     titulo:'CULTIVAR A ESPERANÇA PODE TE FORTALECER  DIANTE DAS DIFICULDADES',
-    link:'filosofia-6.php'
+    link:'filosofia-6'
 },
 {
     img:null,
     titulo:'RELAÇÃO ENTRE A AUTO IMAGEM E QUALIDADE DE VIDA',
-    link:'filosofia-7.php'
+    link:'filosofia-7'
 },
 {
     img:null,
     titulo:'BENEFÍCIOS DA ESPIRITUALIDADE PARA O BEM-ESTAR MENTAL',
-    link:'filosofia-8.php'
+    link:'filosofia-8'
 },
 {
     img:'angel.png',
     titulo:'DIFERENÇA ENTRE ÉTICA E MORAL',
-    link:'filsofia-9.php'
+    link:'filosofia-9'
 },
 {
     img:'relogio3.jpg',
     titulo:'PLANEJAR PARA ECONOMIZAR TEMPO E ENERGIA',
-    link:'filosofia-10.php'
+    link:'filosofia-10'
 },
 {
     img:'meditation.jpg',
     titulo:'Sugestões para uma Boa Tomada de Decisão',
-    link:'filosofia-11.php'
+    link:'filosofia-11'
 },
 {
     img:'self.jpg',
     titulo:'CONHEÇA A SI MESMO',
-    link:'filosofia-12.php'
+    link:'filosofia-12'
 },
 {
     img:null,
     titulo:'QUANDO O REMÉDIO CAUSA DOENÇAS',
-    link:'filosofia-13.php'
+    link:'filosofia-13'
 }
 
 ]
