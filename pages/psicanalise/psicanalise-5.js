7cb64a607c35ac328b57ee41232362f25f16cd35
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-09 22:04:39 -0300

    Create psicanalise-5.js

---
+++ pages/psicanalise/psicanalise-5.js
@@ -0,0 +1,23 @@
+import HeadConfig from "../../include/head"
+import Header from "../../include/header"
+import Menu from "../../include/menu"
+import Footer from "../../include/footer"
+import Principal from "../../supertags/principal"
+import Titulo from "../../supertags/titulo"
+
+function Psicanalise_05 (){
+    return (
+        <>
+        <HeadConfig/>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>PSICANÁLISE: CAMINHO PARA A RESSIGNIFICAÇÃO DO SOFRIMENTO</Titulo>
+            <p>Um dos elementos básicos para o sucesso do processo terapêutico está no estímulo ao desenvolvimento da capacidade de ressignificação dos fatos que provoca sofrimento ao paciente. Isso porque na maioria das vezes o sofrimento está associado à maneira como o indivíduo encara os eventos que estão lhe sucedendo. A ressignificação requer que a pessoa passe a enxergar a situação com mais clareza, desenvolvendo uma compreensão firme a respeito da verdade das coisas, como  de fato ela é, deixando de viver em função de como ela lhe parece ser.</p>
+            <p>Nesse sentido, a análise requer que o indivíduo esteja disposto a revisitar constantemente seus conceitos no sentido de  questionar  as verdades  que ele construiu   sobre si mesmo e sobre os outros. Esse olhar atento e honesto contribui para limpar as impurezas que provocam interpretações equivocadas que muitas vezes  impedem as pessoas de viver com a leveza e suavidade necessária à sua realização e felicidade. Quer saber mais sobre o tratamento em psicanálise? Entre em contato e agende uma avaliação.</p>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+export default Psicanalise_05
