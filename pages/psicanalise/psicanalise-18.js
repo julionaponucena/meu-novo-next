02a76cca5e6b341e614a492127fd2d19414e3a4c
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-10 13:27:40 -0300

    Create psicanalise-18.js

---
+++ pages/psicanalise/psicanalise-18.js
@@ -0,0 +1,23 @@
+import HeadConfig from "../../include/head"
+import Header from "../../include/header"
+import Menu from "../../include/menu"
+import Footer from "../../include/footer"
+import Principal from "../../supertags/principal"
+import Titulo from "../../supertags/titulo"
+
+function Psicanalise_18 (){
+    return (
+        <>
+        <HeadConfig/>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>IDENTIFIQUE A CAUSA DOS PROBLEMAS  DE RELACIONAMENTOS INTERPESSOAIS</Titulo>
+            <p>De maneira geral podemos afirmar os problemas  que uma pessoa enfrenta no seu relacionamento com as outras pessoas são causados pela sua intromissão nas tarefas de vida do outro e  que não lhes dizem respeito. Isso acontece porque ao se intrometer em assuntos cujo encaminhamento diz respeito às escolhas  e perspectivas de vida do outro, o intruso se coloca em posição de superioridade a ele, como se a sua visão de mundo fosse mais clara e precisa. Diante de uma abordagem dessa natureza, a pessoa que está tendo sua vida invadida tende a se sentir desrespeitada e menosprezada,  por ser colocada diante de uma mensagem subliminar de que ela não tem condições de gerenciar sua vida e suas próprias escolhas. O resultado natural diante dessa situação é a adoção de uma defesa reativa, dando origem aos conflitos que tantas vezes observamos nas relações.</p>
+            <p>Se você deseja manter suas boas relações interpessoais o primeiro passo é respeitar as escolhas existenciais realizadas pelo outro, aceitando o fato de que não compete a você fazer com que ele mude o rumo da sua vida. Mudar o estilo de vida é uma tarefa que compete a cada indivíduo, dentro da sua perspectiva história, social e existencial. Ignorar as intenções da pessoa com quem você se relaciona só servirá para causar uma reação intensa por parte dela, com grandes possibilidades de comprometer a harmonia e proatividade necessária para a boa condução de um relacionamento. Essa premissa é válida para as relações de amizade, para os relacionamentos amorosos, para a educação dos filhos e tantas outras relações que travamos em nossa vida. Você se interessou pelo assunto e gostaria de entender melhor sobre o rumo que está conferindo aos seus relacionamentos? Agende uma consulta conosco.</p>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+export default Psicanalise_18
