bcad723b922dae641244dcbf2214a504ec4d5206
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-10 13:28:12 -0300

    Create psicanalise-28.js

---
+++ pages/psicanalise/psicanalise-28.js
@@ -0,0 +1,24 @@
+import HeadConfig from "../../include/head"
+import Header from "../../include/header"
+import Menu from "../../include/menu"
+import Footer from "../../include/footer"
+import Principal from "../../supertags/principal"
+import Titulo from "../../supertags/titulo"
+
+function Psicanalise_28 (){
+    return (
+        <>
+        <HeadConfig/>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>RELAÇÃO ENTRE ARROGÂNCIA E COMPLEXO DE INFERIORIDADE</Titulo>
+            <p>O sentimento de inferioridade é capaz de conduzir o indivíduo a formar sobre si mesmo uma imagem de impotência e de irrealização, provocando-lhe muito sofrimento em função disso, visto que esse sentimento contraria nossa tendência natural a buscar sempre a realização e a sensação de potência dela resultante. Como a permanência do sentimento de inferioridade, vai ficando cada vez mais insustentável na medida em que o tempo passa, muitas pessoas que não conseguem reagir proativamente diante dele, acaba por transformá-lo em algo mais profundo e avassalador para si mesmo que é o desenvolvimento do complexo de inferioridade.</p>
+            <p>Quando não detectado e trabalhado adequadamente o complexo de superioridade pode conduzir a pessoa à adoção de atitudes que visam combatê-lo ou, ao menos, torná-lo  suportável, ocasião em que ele poderá desenvolver mecanismos de defesa inconscientes os quais poderão se manifestar de diversas formas. Em postagens futuras abordaremos os mecanismos de defesa com maior detalhe. Para a presente gostaria de abordar um caminho que o indivíduo cria para combater o seu complexo de inferioridade, ocasião em que ele se conduz para uma margem que aparentemente representa o lado oposto daquilo que ele de fato sente sobre si mesmo: a margem do complexo de superioridade.</p>
+            <p>Na medida em que a pessoa não se sente capaz de superar o seu complexo de inferioridade, nem consegue com ele conviver, ela passa a agir com se fosse superior aos outros, ostentando uma vida de mentiras, na qual ela faz de tudo para que o seu “eu” pareça superior, tanto para si mesmo quanto para os outros. Tal fato é perfeitamente notado nos indivíduos que tendem a desprezar as outras pessoas com base em sua condição social, profissional, intelectual, racial, dentre outras. Percebemos também que algumas pessoas com esse complexo costumam se vangloriar dos seus feitos, quer sejam reais ou ilusórios, desenvolvendo uma narrativa de lembranças das épocas em que sua estrela brilhou mais do que a das outras pessoas, ou mesmo desejando fazer com que suas ideias e projetos se sobressaiam aos dos demais. O mesmo podemos afirmar da necessidade que muitos possuem de perseguir o sucesso a qualquer preço, se alimentando do reforço que lhe é oferecido através dos elogios, ou ainda daquelas pessoas que necessitam estar sempre vestidas com as melhores marcas ou ostentar seu corpo recoberto com as melhores joias para se sentirem em condições de se apresentarem à sociedade. Quer saber mais sobre como você se situa diante de tais fronteiras? Agende uma avaliação conosco.</p>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+export default Psicanalise_28
