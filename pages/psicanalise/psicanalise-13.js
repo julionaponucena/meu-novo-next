73fac9356c8629d6e95bf0964e8e4d582f3bcadc
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-09 22:49:04 -0300

    Create psicanalise-13.js

---
+++ pages/psicanalise/psicanalise-13.js
@@ -0,0 +1,26 @@
+import HeadConfig from "../../include/head"
+import Header from "../../include/header"
+import Menu from "../../include/menu"
+import Footer from "../../include/footer"
+import Principal from "../../supertags/principal"
+import Titulo from "../../supertags/titulo"
+
+function Psicanalise_13 (){
+    return (
+        <>
+        <HeadConfig/>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>A CEGUEIRA PROVOCADA PELO DESEJO DE NÃO ENXERGAR: VISÃO PSICANALÍTICA DA NEGAÇÃO:</Titulo>
+            <p>A vivência de situações que nos provocam sentimentos de extrema vulnerabilidade, insegurança e incertezas como os que o momento de pandemia que atravessamos traz, tem como consequência possível o desenvolvimento de angústias e sofrimentos que podem se tornar insuportáveis para muitos, sobretudo em se tratando de eventos duradouros, cujo desfecho não conseguimos vislumbrar no horizonte. Em situações assim, é comum que muitas pessoas lancem mão de defesas psíquicas inconscientes, visando conseguir uma salvaguarda diante do sofrimento. Essas ações inconscientes são tratadas pela psicanálise como mecanismos de defesa.</p>
+            <p>A psicanálise defende que todos nós desenvolvemos mecanismos de defesa em nosso inconsciente e o utilizamos diante da necessidade de nos proteger de alguma situação que consideramos ameaçadora. Quando nos sentimos ameaçados a colocamos em ação e, mesmo que temporariamente, temos nossas tensões psíquicas internas reduzidas. Um desses mecanismos é a negação.</p>
+            <p>Em situação de negação o que vemos acontecer é uma distorção da realidade, talvez para torná-la suportável, ou ainda por acreditar que deixando de perceber o perigo ou qualquer situação desconfortável, não seremos afetados por ele, tal qual um avestruz que enfia a cabeça no buraco quando se sente ameaçado.</p>
+            <p>Diante de situações que nos coloquem a imperiosa necessidade de adotarmos atitudes que nos garantam a condição de superar a situação que estamos enfrentando, a negação está muito longe de ser a melhor alternativa, sobretudo porque ela nos encaminha para uma condição de embate interior que mais serve para nos aprisionar em um medo improdutivo e disfuncional. Por outro lado, se adotarmos o senso de resolução e agirmos com vistas a pesar nossas atitudes para além das nossas próprias conveniências e desejos, tendemos a nos sentir mais fortalecidos, visto que não nos sentiremos fragilizados pelas cobranças que nos fazemos  quando agimos em sentido contrário à nossa consciência. Essa constatação nos indica a necessidade de se investir na construção de maior consciência acerca dos aspectos que motivam ao desenvolvimento da negação a começar por um exame que cada um pode fazer em relação às reais motivações do seu comportamento, abrindo-se para analisá-las de maneira franca e honesta.</p>
+            <p>Juntamente com a negação outro mecanismo de defesa muito comum cujo uso podemos constatar atualmente é a sublimação. Esse mecanismo se manifesta em forma de comportamentos meritosos, resultantes de desculpas arranjadas pelos indivíduos com o intuito de esconder sua dor ou vergonha diante daquilo que ele executa. Por exemplo, quando sofrem uma grande pressão algumas pessoas costumam se fazer de fortes, desviando suas atenções para a necessidade de alguém virtualmente mais necessitado que ela. Com isso ela deixa de resolver suas questões para ajudar outra pessoa como sinal de desprendimento e amor fraterno. Ocorre que, sem atuar sobre aquilo que a faz sofrer, a pessoa deixa uma ferida aberta que, de uma forma ou outra, tende a se manifestar. Nos tempos da pandemia em que a situação nos exigia o sacrifício do isolamento e do autocuidado como sinal de amor à vida e ao semelhante, muitas pessoas costumavam contornar seus desejos de transgressão das orientações, sublimando a aglomeração em um encontro familiar sob o argumento de que alguém da família estava muito solitário, ou porque não se devia deixar passar batida a comemoração de algum evento festivo. O mesmo aconteceu para com os eventos religiosos, ocasião em que o ato sublime de se encontrar com a comunidade para louvar a Deus serviu como pretexto para a negligência dos cuidados necessários para a preservação do maior bem que Dele recebemos, a própria vida.</p>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+export default Psicanalise_13
