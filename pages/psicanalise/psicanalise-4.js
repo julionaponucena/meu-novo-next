7b20aa4fc3073a7be6bf43d4a53dacfe8117d290
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-09 22:04:36 -0300

    Create psicanalise-4.js

---
+++ pages/psicanalise/psicanalise-4.js
@@ -0,0 +1,21 @@
+import HeadConfig from "../../include/head"
+import Header from "../../include/header"
+import Menu from "../../include/menu"
+import Footer from "../../include/footer"
+import Principal from "../../supertags/principal"
+import Titulo from "../../supertags/titulo"
+function Psicanalise_04 (){
+    return (
+        <>
+        <HeadConfig/>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>TERAPIA COM PSICANÁLISE  PARA ENFRENTAR AS DIFICULDADES EMOCIONAIS</Titulo>
+            <p>Uma das questões recorrentes que os partilhantes deixam transparecer durante a análise é a dificuldade que eles apresentam em identificar seus sentimentos e emoções diante daquilo que estão enfrentando como possível problema em sua vida. Uso aqui o termo possível problema, porque, diante da condição de se visualizar com clareza o que está lhe ocorrendo, o indivíduo pode incorrer em um erro  muito comum que é o de atribuir significados equivocados aos seus eventos existenciais, situação nas quais eventos que notadamente são oportunidade para seu crescimento e fortalecimento, ganham o status de algo penoso e prejudicial. Tudo porque a pessoa teima em observar o que está acontecendo somente com o olhar da objetividade racional e fenomenológica. O trabalho do psicanalista diante de casos assim, é contribuir para que o partilhante seja conduzido ao caminho da análise subjetiva das suas impressões e representações sobre sua vida e tudo aquilo que a circunda, partindo-se do princípio de que existe em cada um de nós um oceano de elementos desconhecidos que, embora inconscientes, teimam em se manifestar constantemente. Quando deixamos nossa vida seguir seu curso guiados pelo desconhecido, corremos o risco de viver como um barco à deriva, sem saber exatamente que rumo dar à nossa viagem existencial. Quer saber mais sobre como funciona uma sessão de psicanálise e como ele pode te ajudar? Entre em contato conosco.</p>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+export default Psicanalise_04
