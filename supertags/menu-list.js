688cfbda0d912346f5c689455d0d50c54d2b5a9a
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-12 15:20:56 -0300

    Update menu-list.js

--- supertags/menu-list.js
+++ supertags/menu-list.js
@@ -1,18 +1,24 @@
 import {FaAngleRight} from "react-icons/fa"
 import { useState } from "react"
-function menuList (props){
+
+
+
+
+function MenuLista (props){
     const [ativar,setAtivar] = useState(false)
     function mostrar (){
         setAtivar(!ativar)
     }
+   
     return (
-    
-            <li className="big-menu"><a className="menu-dropdow" onClick={mostrar}>{props.titulo}<FaAngleRight/></a>
-            <ul className={ativar?"show":"hide"}>
-                {props.children}
-            </ul>
-            </li>
         
+        
+        <li className="big-menu"><a href="#"className="menu-dropdow" onClick={mostrar}><span>{props.titulo}</span><FaAngleRight className={ativar?"rotate icone":"icone"}/></a>
+        <ul className={ativar?"show":"hide"}>
+            {props.children}
+        </ul>
+        </li>
+    
     )
 }
-export default menuList
+export default MenuLista
