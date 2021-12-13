e31585c61453d3f8bcae36175ae6df577cdc2322
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-13 14:18:34 -0300

    Create curso-8.js

---
+++ pages/cursos/curso-8.js
@@ -0,0 +1,42 @@
+import Header from "../../include/header";
+import Principal from "../../supertags/principal";
+import Menu from "../../include/menu";
+import Footer from "../../include/footer";
+import Titulo from "../../supertags/titulo";
+import HeadConfig from "../../include/head";
+
+function Curso_08 (){
+    return (
+        <>
+        <HeadConfig/>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>Autoconhecimento através da História da Filosofia</Titulo>
+            <h3>Justificativa:</h3>
+            <p>Como saber o que se passa nas mentes alheias? O que podemos esperar do outro? O que podemos esperar de nós mesmos? Qual o nosso papel na constituição do mundo em que vivemos? A filosofia, desde suas origens, destina-se a refletir sobre essas e outras questões da vida.</p>
+            <p>Os temas e problemas trabalhados no decorrer da História da Filosofia sempre estiveram vinculados aos temas e problemas do cotidiano de cada época. Seus instrumentos de leitura de textos e contextos foram utilizados como forma de lidar com as questões suscitadas no decorrer da História. Diante das questões de nosso tempo, pode a filosofia nos oferecer instrumentos para lidar com nossas inquietações?</p>
+            <p>O curso de Introdução à Filosofia apresentará a seus participantes, a partir de leituras e conversações, os temas, problemas e métodos filosóficos desenvolvidos no decorrer da História da Filosofia, estabelecendo uma conversação entre as abordagens históricas e as questões da vida cotidiana. Assim sendo, esse  consiste numa apresentação do universo filosófico, visitando as principais correntes e questões propostas na história da Filosofia, e avaliando sua atualidade, sua necessidade e seu papel no contexto contemporâneo, de forma a provocar a reflexão acerca das questões cotidianas.</p>
+            <h3>Objetivo Geral:</h3>
+            <p>Provocar a reflexão fundamentada acerca das questões cotidianas, apropriando-se de um referencial teórico filosófico que propicie as bases para o desenvolvimento da leitura e da atitude filosóficas, assim como o exercício da reflexão crítica.</p>
+            <h3>Programa:</h3>
+            <h2>Módulo 1: O que é filosofia e qual o seu papel na sociedade contemporânea.</h2>
+            <p>O que caracteriza uma reflexão filosófica? Quais as distinções entre filosofia e outros saberes? Acompanhando a história é possível encontrar diferentes definições para filosofia. O que elas têm em comum? Qual a relação entre filosofia e história? E entre filosofia e cotidiano? Há lugar ou papel para a filosofia no século XXI?</p>
+            <p>Essas e outras questões que se referem ao conceito de filosofia na contemporaneidade e seu possível papel social serão suscitadas no primeiro módulo do curso. Partindo da filosofia em seu surgimento, serão abordadas algumas definições no percurso histórico, culminando na questão &ldquo;o que é filosofia?&rdquo; em sua abordagem contemporânea.</p>
+            <h2>Módulo 2: Como sabemos o que sabemos?</h2>
+            <p>Loucura, desconfiança, desprezo estão presentes nas atitudes que nos amedrontam no século XXI. Não sabemos o que pensam os outros, não sabemos do que são capazes os outros. Como ter acesso às crenças alheias? Muitas vezes julgamos saber, mas não investigamos profundamente o que sabemos. Nosso saber pode estar fundamentado em crenças falsas, infundadas. Como sabemos o que sabemos? Essa é uma antiga questão da filosofia.</p>
+            <p>O que é um problema filosófico? Que métodos utilizar para abordá-lo? Como abordar uma questão filosoficamente? São as questões do módulo 2 de nosso curso.</p>
+            <h2>Módulo 3: A filosofia e sua história I</h2>
+            <p>Breve panorama da história da filosofia antiga e medieval. Contextos, principais questões, autores, métodos e abordagens, construindo uma linha do tempo através dos textos.</p>
+            <h2>Breve panorama da história da filosofia antiga e medieval. Contextos, principais questões, autores, métodos e abordagens, construindo uma linha do tempo através dos textos.</h2>
+            <p>Breve panorama da história da filosofia moderna e contemporânea. Contextos, principais questões, autores, métodos e abordagens, construindo uma linha do tempo através dos textos.</p>
+            <h3>Metodologia:</h3>
+            <p>Apresentação dos principais problemas e métodos filosóficos no decorrer da história da filosofia. Leitura de textos filosóficos e leitura de contextos com abordagem filosófica. Conversações sobre as questões filosóficas contemporâneas, fundamentadas nos subsídios apresentados na história da filosofia.</p>
+            <h3>Metodologia:</h3>
+            <p>O curso se destina a todos aqueles que se sentirem motivados a desenvolver uma busca para dentro de si mesmo, utilizando-se do conhecimento milenar propiciado pela construção filosófica ao longo da nossa história.</p>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+export default Curso_08
