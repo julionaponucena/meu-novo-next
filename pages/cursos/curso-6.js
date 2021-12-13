124f953f09d7b4c51f51e5b30ce61fca7796e9a2
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-13 12:57:06 -0300

    Create curso-6.js

---
+++ pages/cursos/curso-6.js
@@ -0,0 +1,100 @@
+import Header from "../../include/header";
+import Principal from "../../supertags/principal";
+import Menu from "../../include/menu";
+import Footer from "../../include/footer";
+import Titulo from "../../supertags/titulo";
+import HeadConfig from "../../include/head";
+
+function Curso_06 () {
+    return (
+        <>
+        <HeadConfig/>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>A EVOLUÇÃO NA GESTÃO DE PESSOAS</Titulo>
+            <h2>1º ENCONTRO: </h2>
+            <p><b>DURAÇÃO: </b>4 horas</p>
+            <h2>TEMA: ARISTÓTELES E A PEDAGOGIA DA VIRTUDE</h2>
+            <h3>Objetivo:</h3>
+            <p>Oferecer subsídios para a reflexão acerca do papel da educação na visão de Aristóteles, aplicada à atualidade, como forma de construir o indivíduo virtuoso, condição necessária para o exercício de uma existência plena. Espera-se que a partir do tema desenvolvido os gestores passem a compreender (ou reforcem sua compreensão) que a função por excelência da escola está no aspecto pedagógico.</p>
+            <h3>CONTEÚDO</h3>
+            <p><b>- A essência e a existência em Aristóteles</b> (discutem-se aqui as deturpações que podemos promover nas instituições, quando não concebemos a real razão da sua existência).</p>
+            <p><b> A distinção entre potência e ato </b>(discutem-se aqui as ações que podem levar o indivíduo a desenvolver plenamente suas potencialidades e aquelas que, quando não aplicadas adequadamente, limitam ou destroem as potencialidades).</p>
+            <p><b>Ética à Nicômaco: </b> estudo da obra do pensador na qual se discute o objeto de agir do ser humano, como:</p>
+            <ul className="lista-conteudo">
+                <li>as virtudes.</li>
+                <li>a estrutura do ato moral.</li>
+                <li>as virtudes morais.</li>
+                <li>as virtudes intelectuais.</li>
+            </ul>
+            <h2>2º ENCONTRO:</h2>
+            <p><b>DURAÇÃO: </b>4 horas</p>
+            <h2>TEMA: NIETZSCHE EDUCADOR</h2>
+            <h3>Objetivo:</h3>
+            <p>Oferecer subsídios para a reflexão acerca do papel da educação na visão de Nietzsche , aplicada à atualidade, a fim de conduzir à reflexão a respeito da educação como instrumento para a superação da nossa condição comum, fonte necessária portanto, para a criação do ser humano pleno de vigor</p>
+            <p><b>Da Genealogia da moral de  Nietzsche:</b> Estudo de  conceitos como vingança, espírito de vingança, má consciência, ascetismo estarão presentes e estudados à luz do pensador. Tentaremos também apontar de, maneira breve, como as temáticas de valoração e transvaloração se articulam com os conceitos de vontade de poder e eterno retorno. Estudaremos também sua tese acerca da educação como forma de superação do homem comum, condição necessária para a criação do modelo de homem perfeito, o super-homem.</p>
+            <h2>3º  ENCONTRO</h2>
+            <p><b>DURAÇÃO: </b>4 horas</p>
+            <h2>TEMA: O MUNDO COMO VONTADE E REPRESENTAÇÃO – DE PROTÁGORAS A SCHOPENHAUER</h2>
+            <h3>Objetivo:</h3>
+            <p>Refletir sobre as diversas representações de um mesmo mundo que podem ser oferecidas pelos diferentes indivíduos com os quais convivemos em nosso cotidiano, como forma de desenvolvermos mecanismos que nos tornem capazes de distinguir entre aquilo que pode ser relativizado daquilo que não pode ser condicionado à livre interpretação (o aspecto prático está no fato de que os gestores, no convívio e liderança de diversos indivíduos, possam lançar mão desse referencial para interpretar suas manifestações a respeito do seu trabalho e da sua própria vida. A partir daí esperamos   oferecer-lhes subsídios para manter uma linha de conduta segura mesmo diante da diversidade de comportamento e de ações dos seus liderados).</p>
+            <h3>CONTEÚDO:</h3>
+            <h4>OS PENSADORES SOFISTAS: “o homem é a medida de todas as coisas”.
+            </h4>
+            <h4>-  A verdade pode ser absoluta?</h4>
+            <h4>O relativismo pode ser aplicado em todas as circunstâncias?</h4>
+            <h4>SCHOPENHAUER: distinção entre o que nos aparece e o que existiria em si mesmo.</h4>
+            <h2>4º ENCONTRO:</h2>
+            <p><b>DURAÇÃO: </b>4 horas</p>
+            <h2>TEMAS: RELAÇÕES INTERPESSOAIS E PROCESSO DE COMUNICAÇÃO</h2>
+            <h3>Objetivo:</h3>
+            <p>Seguindo a linha desenvolvida no encontro anterior, pretende-se nortear o estudo para a criação de condições de comunicação eficiente, afim de que a interpretação daquilo que se ouve seja coerente  com a mensagem transmitida.</p>
+            <h3>CONTEÚDO:</h3>
+            <h4>- A linguagem na visão de Platão: veneno, cosmético e medicamento.</h4>
+            <h4>- Objetividade e subjetividade na comunicação.</h4>
+            <h4>- Entendimento na comunicação.</h4>
+            <h4>- Comunicação verbal e não verbal.</h4>
+            <h4>Fatores intervenientes – atitudes.</h4>
+            <h4>- Fatores intervenientes – situação.</h4>
+            <h4>Fatores intervenientes – conteúdo e forma.</h4>
+            <h4>- fatores intervenientes – valores e costumes.</h4>
+            <h2>5º ENCONTRO: </h2>
+            <p><b>DURAÇÃO: </b>4 horas</p>
+            <h2>TEMA: TEORIAS E PRÁTICAS MOTIVACIONAIS (PARTE I)</h2>
+            <h3>Objetivo:</h3>
+            <p>Compreender, a partir do estudo das teorias sobre as necessidades, os mecanismos motivacionais que desenvolvemos ou deixamos de desenvolver para a realização das nossas tarefas cotidianas. A partir de tal compreensão, esperamos oferecer os subsídios necessários para que o gestor atue no sentido de tornar o ambiente de trabalho apropriado para o seu desenrolar.</p>
+            <h3>CONTEÚDO:</h3>
+            <h4>Pascal: “o coração tem razões que a própria razão desconhece”.</h4>
+            <h4>Teoria de Maslow – necessidades primárias.</h4>
+            <h4>Teoria de Maslow – necessidades secundárias.</h4>
+            <h4>Teoria de Herzberg.</h4>
+            <h4>Teoria e McClelland.</h4>
+            <h4>Teoria da expectativa e da equidade.</h4>
+            <h4>Reconhecimento.</h4>
+            <h2>6º ENCONTRO:</h2>
+            <p><b>DURAÇÃO: </b>4 horas</p>
+            <h2>TEMA: TEORIAS E PRÁTICAS MOTIVACIONAIS (PARTE II)</h2>
+            <h3> Mecanismos de defesa:</h3>
+            <h4>Mecanismos Psicológicos.</h4>
+            <h4>Racionalização, fantasia, projeção.</h4>
+            <h4>Deslocamento, simbolismo, sublimação.</h4>
+            <h4>Isolamento, compensação, regressão.</h4>
+            <h4>- Apatia, generalização, somatização.</h4>
+            <h2>7º ENCONTRO:</h2>
+            <p><b>DURAÇÃO: </b>4 horas</p>
+            <h2>TEMA 1: AUTOGENIA EM FILOSOFIA CLÍNICA</h2>
+            <h3>CONTEÚDO:</h3>
+            <p>Definição de Autogenia; Questões Autogênicas;Modelo organizacional, modelo funcional; Níveis, Vizinhança; Aspectos mais e menos mecânicos; Padrões Autogênicos; Um Significado ao modificar a Autogenia; Exercício de Identificação; Propensões Inerciais; Variações, Flutuações; Algumas confusões em torno da Autogenia; Combinações: interseções entre Autogenias; Dilemas: Sócrates, Gandhi.; Analítica de Linguagem e Autogenia; Alguns cuidados com a alma;Introdução a um Modelo de Mente; O Mundo como Representação, o Mundo como Construção.</p>
+            <p><b>Para informação:  </b>autogenia diz respeito aos aspectos organizacionais e de relação entre os tópicos da nossa Estrutura de Pensamento.</p>
+            <h2>8º ENCONTRO:</h2>
+            <p><b>DURAÇÃO: </b>4 horas</p>
+            <h2>TEMA 2: EMOÇÕES EM FILOSOFIA CLÍNICA</h2>
+            <h3>CONTEÚDO:</h3>
+            <p>Introdução às emoções; Elementos gerais sobre as Emoções; Definição e Localização; Andamento da Afetividade; Algumas complexidades em torno das emoções; Aspectos Dinâmicos; Exatidão e Aproximação; Emoções e Ambiente; Lidando com Emoções - parte I; Lidando com Emoções - parte II; - Lidando com Emoções - parte III; Síntese sobre as Emoções.</p>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+export default Curso_06
