cc5b83e02d487ccd2212572878aff5aa86f977c7
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-13 11:56:30 -0300

    Create curso-1.js

---
+++ pages/cursos/curso-1.js
@@ -0,0 +1,50 @@
+import Header from "../../include/header";
+import Principal from "../../supertags/principal";
+import Menu from "../../include/menu";
+import Footer from "../../include/footer";
+import Titulo from "../../supertags/titulo";
+import HeadConfig from "../../include/head";
+
+function Curso_01 (){
+    return (
+        <>
+        <HeadConfig/>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>CONTRIBUIÇÃO DA PSICANÁLISE PARA A EDUCAÇÃO EMOCIONAL DOS EDUCADORES</Titulo>
+            <h2>JUSTIFICATIVA</h2>
+            <p>Muito se tem falado e debatido sobre a importância do preparo técnico do professor, afim de que ele possa exercer com eficiência o seu nobre papel na condução do trabalho pedagógico. Juntamente com essa demanda existe uma outra que, embora nem sempre é colocada de maneira explícita, ela se apresenta a qualquer profissional como uma condição básica para o sucesso do seu trabalho que é a condição de gerenciar suas próprias emoções. No caso do professor essa questão é ainda mais importante, porque é dele a responsabilidade por administrar um contingente grande de estudantes, os quais pela singularidade que lhes é própria, transformam a sala de aula em um turbilhão de emoções e, em muitos casos, de conflitos. Diante desse cenário, fica cada vez mais evidente que o professor que possui a capacidade de gerenciar suas emoções tende a sair-se melhor na condução das relações que a ele compete administrar.  O presente curso pretende ser uma ferramenta para que o profissional se fortaleça emocionalmente e se prepare assim para os desafios que sua profissão lhe colocar.</p>
+            <h2>OBJETIVO</h2>
+            <p>O  curso  apresentará as ferramentas que auxiliam na gestão do equilíbrio emocional, através de estratégias que articulam teoria e vivências, fazendo com que os indivíduos se tornem menos vulneráveis às circunstâncias cotidianas.  Assim ele pretende:</p>
+            <ul class="lista-conteudo">
+                <li>Auxiliar na compreensão  do funcionamento das emoções e sentimentos e como manter o equilíbrio emocional nas diversas situações da vida.</li>
+                <li>Auxiliar  a mapear os acionadores das emoções e a lidar melhor com a Inteligência emocional.</li>
+                <li>Desenvolver seu potencial e construir lastro nas cinco competências da IE.</li>
+                <li>Fornecer técnicas para aumentar a capacidade de influenciar as pessoas afim de  obter a colaboração em direção aos resultados desejados. </li>
+            </ul>
+            <h2>A QUEM SE DESTINA</h2>
+            <p>Professores, demais trabalhadores da educação ou pessoas que tenham interesse em buscar o seu amadurecimento emocional.</p>
+            <h2>METODOLOGIA</h2>
+            <p>As aulas ocorrem de forma dinâmica e participativa, baseada no processo de aprendizagem vivencial, utilizando apresentações com recursos multimídia, exercícios diagnósticos, estudos e apresentação de casos.</p>
+            <h2>PROGRAMA</h2>
+            <ul>
+                <li>Entendendo nossa estrutura psíquica à luz da Psicanálise.</li>
+                <li>Arthur Schopenhauer: o mundo como vontade e representação.</li>
+                <li>Nietzsche: quando o obstáculo se torna nosso aliado.</li>
+                <li>O fenômeno da transferência nas relações interpessoais: como não cair nessa armadilha.</li>
+                <li>Idealização, experiência e desencanto: como lidar com essa trilogia.</li>
+                <li>Inteligência emocional: conceitos fundamentais.</li>
+                <li>Tipos de Inteligência Emocional.</li>
+                <li>Competência da Inteligência Emocional.</li>
+                <li>As aptidões emocionais básicas.</li>
+                <li>A Inteligência Emocional na sala de aula: apoio ao professor.</li>
+                <li>A postura do professor diante das emoções dos estudantes.</li>
+                <li>A conexão entre a escola e a emoção dos estudantes.</li>
+            </ul>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+export default Curso_01
