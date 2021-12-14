c9151e19030f3e0f3ffbad9614e4303bd7933edd
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-14 16:05:37 -0300

    Update hiperlinks.js

--- scripts/hiperlinks.js
+++ scripts/hiperlinks.js
@@ -2,13 +2,16 @@ import Image from "next/image";
 import style from "../styles/modulos/hiper-menu.module.css"
 import Link from "next/link";
 function HiperLink ({artigo,nomeArtigo}){
-let titulo
+function animar(a,b){
+  console.log(b)
+}
   return(<>
   
   {artigo.map(item=> (<>
+    
     <Link href={"/"+nomeArtigo+"/"+item.link}>
           <a className={style.hiperLink}>
-          <Image className={style.foto} src={"/"+item.img} width={360} height={240}/>
+          <Image placeholder="blur" blurDataURL={"/"+item.img} className={style.foto} src={"/"+item.img} width={360} height={240}/>
         <p>{item.titulo.toUpperCase()}</p>
       </a>
     </Link>
