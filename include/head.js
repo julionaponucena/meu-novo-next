dd30da972043a9b63d9e1a82b0cbd4d9be4819ab
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-14 16:57:05 -0300

    Update head.js

--- include/head.js
+++ include/head.js
@@ -4,7 +4,7 @@ function HeadConfig(){
   return(   
     <Head>
        <link rel="shortcut icon" href="/logo-mente.png" type="imagem/x-icon"/>
-       <title>Meu primeiro Site</title> 
+       <title>Restauramente</title> 
     </Head>
     )
 }
