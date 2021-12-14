a2741c33bc462baa07c4f113455dc9a51b07cf84
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-14 16:44:32 -0300

    Update footer.js

--- include/footer.js
+++ include/footer.js
@@ -4,17 +4,8 @@ function Footer (){
     return(
       
         <><footer>
-            <a target="_blank" href="https://api.whatsapp.com/send?phone=5511971252865" className={style.whatsapp}>
-                <Image src="/whatsapp.png" width={70} height={70}/>    
-            </a>
-            <div className="conteudo-rodape">
-                <p>Siga-nos também nas redes sociais</p>
-                <a target="_blank" href="https://www.instagram.com/psicanalistaaparecidoalcantara/">
-                </a>
-                <a target="_blank" href="https://www.facebook.com/isaltosp">
-                   
-                </a>
-            </div>
+          <p className={style.direitoUso}>Copyright © 2021 |Júlio Naponucena de Alcântara</p>
+            
         </footer><a className="terapia" target="_blank" href="https://api.whatsapp.com/send?phone=5511971252865">
                 <span> Agende sua sessão online</span>
             </a><a target="_blank" className="whatsapp" href="https://api.whatsapp.com/send?phone=5511971252865"> </a></>
