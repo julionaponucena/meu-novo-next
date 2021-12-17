5751f629c64a7c9f4d34ca5a5e1bf9f13523e563
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-17 17:47:29 -0300

    Update footer.js

--- include/footer.js
+++ include/footer.js
@@ -3,12 +3,21 @@ import Image from 'next/image'
 function Footer (){
     return(
       
-        <><footer>
+        <><footer className={style.footer}>
+          <div className={style.footerContainer}>
+            <div className={style.containerItem}>
+              <p className={style.nomeSite}>Site Restauramente</p><Image src="/logo-mente.png" width={30} height={30}/>
+            </div>
+            <div className={style.descricaoAutor}>
+              <p>Psicanalista Aparecido Soares de Alcântara Filho. Faz sessões de forma online. Para ter acesso a mais infornações entre em contato pelo número (11)971252865</p>
+            </div>
+          </div>
+
           <p className={style.direitoUso}>Copyright © 2021 |Júlio Naponucena de Alcântara</p>
             
-        </footer><a className="terapia" rel="noreferrer external" target="_blank" href="https://wa.me/5511971252865">
+        </footer><a className={style.terapia}rel="noreferrer external" target="_blank" href="https://wa.me/5511971252865">
                 <span> Agende sua sessão online</span>
-            </a><a target="_blank"rel="noreferrer external" className={style.whatsapp} href="https://wa.me/5511971252865"> <Image  src="/whatsapp.png" width={70} height={70}/></a></>
+            </a><a target="_blank"rel="noreferrer external" className={style.whatsapp} href="https://wa.me/5511971252865"> <Image  src="/whatsapp.png" width={70} height={70} quality={50}/></a></>
     
     )
 }
