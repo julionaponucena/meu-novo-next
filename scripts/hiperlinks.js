af28a0ff193e0f0d55d3e92725e029b704a36aa6
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-17 17:47:36 -0300

    Update hiperlinks.js

--- scripts/hiperlinks.js
+++ scripts/hiperlinks.js
@@ -11,7 +11,7 @@ function animar(a,b){
     
     <Link href={"/"+nomeArtigo+"/"+item.link}>
           <a className={style.hiperLink}>
-          <Image placeholder="blur" blurDataURL={"/"+item.img} className={style.foto} src={"/"+item.img} width={360} height={240}/>
+          <Image placeholder="blur" blurDataURL={"/"+item.img} className={style.foto} src={"/"+item.img} width={360} height={240} quality={50}/>
         <p>{item.titulo.toUpperCase()}</p>
       </a>
     </Link>
