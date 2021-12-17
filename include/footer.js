e76c9e52c02da9c1cc7d3d6c500f8caa34d06305
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-17 15:40:51 -0300

    Update footer.js

--- include/footer.js
+++ include/footer.js
@@ -6,9 +6,9 @@ function Footer (){
         <><footer>
           <p className={style.direitoUso}>Copyright © 2021 |Júlio Naponucena de Alcântara</p>
             
-        </footer><a className="terapia" target="_blank" href="https://api.whatsapp.com/send?phone=5511971252865">
+        </footer><a className="terapia" rel="noreferrer" target="_blank" href="https://api.whatsapp.com/send?phone=5511971252865">
                 <span> Agende sua sessão online</span>
-            </a><a target="_blank" className="whatsapp" href=""> </a></>
+            </a><a target="_blank"rel="noreferrer" className="whatsapp" href="https://wa.me/5511971252865"> </a></>
     
     )
 }
