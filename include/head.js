c6f9a6ca924874c1e3ae8cacbe81ec132dcb8afa
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-17 19:03:09 -0300

    Update head.js

--- include/head.js
+++ include/head.js
@@ -1,8 +1,10 @@
+import { Html } from "next/document"
 import Head from "next/head"
 
 function HeadConfig(){
   return(   
     <Head>
+      <Html lang="pt-br"/>
        <link rel="shortcut icon" href="/logo-mente.png" type="imagem/x-icon"/>
        <title>Restauramente</title> 
     </Head>
