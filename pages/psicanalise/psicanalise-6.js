0a3e2c6c9b8ee646b532a632865b906fcdc23953
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-21 22:15:38 -0300

    Update psicanalise-6.js

--- pages/psicanalise/psicanalise-6.js
+++ pages/psicanalise/psicanalise-6.js
@@ -1,4 +1,4 @@
-import HeadConfig from "../../include/head"
+import Head from "next/head"
 import Header from "../../include/header"
 import Menu from "../../include/menu"
 import Footer from "../../include/footer"
@@ -8,7 +8,11 @@ import Titulo from "../../supertags/titulo"
 function Psicanalise_06 (){
     return(
         <>
-        <HeadConfig/>
+        <Head>
+            <title>Depressão e Ansiedade:O Valor de Viver no Momento Presente</title>
+            <meta name="keywords" name="ansiedade excesso de futuro,excesso de passado"/>
+            
+        </Head>
         <Header/>
         <Menu/>
         <Principal>
