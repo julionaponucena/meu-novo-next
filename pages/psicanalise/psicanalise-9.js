78a432d71cd67c6bd86b3b4b3fad2767dbf65c45
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-22 14:30:03 -0300

    Update psicanalise-9.js

--- pages/psicanalise/psicanalise-9.js
+++ pages/psicanalise/psicanalise-9.js
@@ -1,4 +1,4 @@
-import HeadConfig from "../../include/head"
+import Head from "next/head"
 import Header from "../../include/header"
 import Menu from "../../include/menu"
 import Footer from "../../include/footer"
@@ -8,7 +8,10 @@ import Titulo from "../../supertags/titulo"
 function Psicanalise_09 (){
     return (
         <>
-        <HeadConfig/>
+        <Head>
+            <title>Mentes Sadias Geram Corpos Saudaveis</title>
+            <meta name="keywords" content="viver em paz"/>
+        </Head>
         <Header/>
         <Menu/>
         <Principal>
