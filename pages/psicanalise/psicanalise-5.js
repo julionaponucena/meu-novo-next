a4cbe34d01fca69794f83cf7df8456619d2c0ddf
Júlio Alcântara <julio.naponucena@hotmail.com>
2021-12-21 22:15:35 -0300

    Update psicanalise-5.js

--- pages/psicanalise/psicanalise-5.js
+++ pages/psicanalise/psicanalise-5.js
@@ -1,4 +1,4 @@
-import HeadConfig from "../../include/head"
+import Head from "next/head"
 import Header from "../../include/header"
 import Menu from "../../include/menu"
 import Footer from "../../include/footer"
@@ -8,7 +8,10 @@ import Titulo from "../../supertags/titulo"
 function Psicanalise_05 (){
     return (
         <>
-        <HeadConfig/>
+        <Head>
+            <title>Caminho para a Ressigificação do Sofrimento</title>
+            <meta name="keywords" content="sofrimento psiquico"/>
+        </Head>
         <Header/>
         <Menu/>
         <Principal>
