5322c5f22a4e4c21262e29c38846f3f0a00eaa06
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-01-03 10:00:34 -0300

    Update psicanalise-17.js

--- pages/psicanalise/psicanalise-17.js
+++ pages/psicanalise/psicanalise-17.js
@@ -1,4 +1,4 @@
-import HeadConfig from "../../include/head"
+import Head from "next/head"
 import Header from "../../include/header"
 import Menu from "../../include/menu"
 import Footer from "../../include/footer"
@@ -8,7 +8,10 @@ import Titulo from "../../supertags/titulo"
 function Psicanalise_17 (){
     return(
         <>
-        <HeadConfig/>
+        <Head>
+            <title>Superioridade Disfarçada em Forma de Críticas e  Elogios</title>
+            <meta name="keywords" content="como lidar com pessoas arrogantes"/>
+        </Head>
         <Header/>
         <Menu/>
         <Principal>
