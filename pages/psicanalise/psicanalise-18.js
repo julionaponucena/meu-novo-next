2649c392d4292d89b0b5a473fbbed68de1a3c9f1
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-01-03 10:00:38 -0300

    Update psicanalise-18.js

--- pages/psicanalise/psicanalise-18.js
+++ pages/psicanalise/psicanalise-18.js
@@ -1,4 +1,4 @@
-import HeadConfig from "../../include/head"
+import Head from "next/head"
 import Header from "../../include/header"
 import Menu from "../../include/menu"
 import Footer from "../../include/footer"
@@ -8,7 +8,10 @@ import Titulo from "../../supertags/titulo"
 function Psicanalise_18 (){
     return (
         <>
-        <HeadConfig/>
+        <Head>
+            <title>Identifique a Causa Dos Problemas  De Relacionamentos Interpessoais</title>
+            <meta name="keywords" content="problemas entre amigos,problemas no relacionamento"/>
+        </Head>
         <Header/>
         <Menu/>
         <Principal>
