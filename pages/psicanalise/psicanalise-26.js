9d26a923e346a99bc78be125c8e5a60516a8a739
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-01-03 19:07:31 -0300

    Update psicanalise-26.js

--- pages/psicanalise/psicanalise-26.js
+++ pages/psicanalise/psicanalise-26.js
@@ -1,4 +1,4 @@
-import HeadConfig from "../../include/head"
+import Head from "next/head"
 import Header from "../../include/header"
 import Menu from "../../include/menu"
 import Footer from "../../include/footer"
@@ -8,7 +8,10 @@ import Titulo from "../../supertags/titulo"
 function Psicanalise_26 (){
     return (
         <>
-        <HeadConfig/>
+        <Head>
+            <title>Ressignifique Seu Olhar Diante Da Vida e se Livre da Competição Compulsiva</title>
+            <meta name="keywords" content="ressignificar a vida"/>
+        </Head>
         <Header/>
         <Menu/>
         <Principal>
