e00aee3833a9c67631ca64b823b72573f0a80ee3
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-01-03 19:08:57 -0300

    Update psicanalise-43.js

--- pages/psicanalise/psicanalise-43.js
+++ pages/psicanalise/psicanalise-43.js
@@ -1,4 +1,4 @@
-import HeadConfig from "../../include/head"
+import Head from "next/head"
 import Header from "../../include/header"
 import Menu from "../../include/menu"
 import Footer from "../../include/footer"
@@ -8,7 +8,10 @@ import Titulo from "../../supertags/titulo"
 function Psicanalise_43 (){
     return (
         <>
-        <HeadConfig/>
+        <Head>
+            <title></title>
+            <meta name="keywords" content="dor existencial"/>
+        </Head>
         <Header/>
         <Menu/>
         <Principal>
