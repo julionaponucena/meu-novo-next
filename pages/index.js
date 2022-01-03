45a5e56776c79f997f3375d80098bfa51f834048
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-01-03 19:07:04 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,19 @@ import Image from 'next/image'
 import Footer from '../include/footer'
 import Header from '../include/header'
 import Menu from '../include/menu'
-import UseTempo from '../hooks/usetempo'
-import { useState} from 'react'
-import React from 'react'
+
+
 
 import Link from 'next/dist/client/link'
 
 import ContainerTitulo from '../supertags/titulo-secao'
 function home (){
-    const tempo = UseTempo(0,1000,5)
+    
     
     
  return(<>  
             <Head>
-                <title>Se conheça melhor</title>
+                <title>Restauramente</title>
                 <meta name="keywords" content="alma humana,autoconhecimento"/>
         
             </Head>
@@ -25,7 +24,7 @@ function home (){
             <main className="principal">
             <section className="container">
                 
-                <h1>Seja bem-vindo ao nosso site {tempo} </h1>
+                <h1>Seja bem-vindo ao nosso site</h1>
                 <p>Aqui você encontrará conteúdos que tratam dos diversos aspectos da alma humana, como suas emoções, seus sentimentos, pensamentos e atitudes deles advindos, assuntos desenvolvidos com o objetivo de auxiliá-lo em sua busca por entender o ser humano na sua complexidade e inteireza. Além disso disponibilizamos canais de acesso para que você entre em contato com o especialista para sanar algumas dúvidas e também para agendar suas sessões de psicanálise, quer seja para trabalhar algumas questões específicas que estejam dificultando sua trajetória existencial, quer seja para trabalhar temas que possam auxiliá-lo em seu autoconhecimento. Desejamos que você tenha uma ótima e proveitosa viagem pelo site e para dentro de si mesmo.</p>
                       
                       
