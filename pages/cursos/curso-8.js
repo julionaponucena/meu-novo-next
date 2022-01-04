14551e8ffa0563035c9434d3adb733caadf7a245
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-01-04 17:59:54 -0300

    Update curso-8.js

--- pages/cursos/curso-8.js
+++ pages/cursos/curso-8.js
@@ -3,12 +3,15 @@ import Principal from "../../supertags/principal";
 import Menu from "../../include/menu";
 import Footer from "../../include/footer";
 import Titulo from "../../supertags/titulo";
-import HeadConfig from "../../include/head";
+import Head from "next/head";
 
 function Curso_08 (){
     return (
         <>
-        <HeadConfig/>
+        <Head>
+            <title></title>
+            <meta name="keywords" content="autoconhecimento na filosofia"/>
+        </Head>
         <Header/>
         <Menu/>
         <Principal>
