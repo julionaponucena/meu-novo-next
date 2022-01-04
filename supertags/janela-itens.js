511655699e0792889d23e48bbfc9322676662557
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-01-04 20:22:33 -0300

    Update janela-itens.js

--- supertags/janela-itens.js
+++ supertags/janela-itens.js
@@ -1,1 +1,3 @@
-const imagensItens = {}
+const imagensItens = ['anuncio-curso.png','anuncio-pais.jpg','foto 2.jpeg','foto 3.jpeg','galera-agrupada.jpg','galera-assistindo.JPG','galera-circulo.jpg','galera-dialogo.jpg','galera-exercitando.jpg','galera-lanche.jpg','galera-linha2.jpg','galera-mesa.jpg','galera-roda.jpg','galera-vendada.jpg','moca-vendada.jpg','palestra.jpg','palestra2.jpg','palestra3.jpg','palestra4.jpg','praticando-atividade.jpg','professor-aula.jpg','professor-aula2.jpg','quadro-avisos.jpg']
+
+export default imagensItens
