8b4cdc37196020509a450f18881cfb3424c049d2
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-01-04 16:48:52 -0300

    Update curso-3.js

--- pages/cursos/curso-3.js
+++ pages/cursos/curso-3.js
@@ -3,12 +3,15 @@ import Principal from "../../supertags/principal";
 import Menu from "../../include/menu";
 import Footer from "../../include/footer";
 import Titulo from "../../supertags/titulo";
-import HeadConfig from "../../include/head";
+import Head from "next/head";
 
 function Curso_03 (){
     return (
         <>
-        <HeadConfig/>
+        <Head>
+            <title>Grupo de Pais: Desenvolvendo Valores na Educação</title>
+            <meta name="keywords" content=""/>
+        </Head>
         <Header/>
         <Menu/>
         <Principal>
