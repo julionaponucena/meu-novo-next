06adbf71f48074ace78711f10ee68945a365c326
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-01-04 20:22:35 -0300

    Update janela.js

--- supertags/janela.js
+++ supertags/janela.js
@@ -1,8 +1,21 @@
 import UseTempo from "../hooks/usetempo";
-
+import imagensItens from "./janela-itens";
+import Image from "next/image";
 function JanelaSlide (){
-    const [tempo,setTempo] = UseTempo(0,1000,5)
-    return tempo
+    const [tempo,setTempo] = UseTempo(0,3000,imagensItens.length-1)
+    function voltar (){
+        setTempo(tempo-=1)
+    }
+    function avancar (){
+        setTempo(tempo+=1)
+    }
+    return(
+        <div>
+            <button onClick={voltar}>{"<"}</button>
+            <Image src={`/slide/${imagensItens[tempo]}`} width={300} height={300}/>
+            <button onClick={avancar}>{'>'}</button>
+        </div>
+    )
 }
 
 export default JanelaSlide
