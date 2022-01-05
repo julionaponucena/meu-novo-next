1403f95b17b8c0251e1cfe2fc48d60bed0790cbd
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-01-05 19:27:15 -0300

    Update janela.js

--- supertags/janela.js
+++ supertags/janela.js
@@ -1,14 +1,26 @@
 import UseTempo from "../hooks/usetempo";
 import imagensItens from "./janela-itens";
 import Image from "next/image";
+
 function JanelaSlide (){
     const [tempo,setTempo] = UseTempo(0,3000,imagensItens.length-1)
     function voltar (){
-        setTempo(tempo-=1)
+        if (tempo <= 0 ){
+            setTempo(imagensItens.length-1)
+        }
+        else{
+            setTempo(tempo-1)
+        }
     }
     function avancar (){
-        setTempo(tempo+=1)
+        if(tempo >=imagensItens.length-1){
+            setTempo(0)
+        }
+        else{ 
+        setTempo(tempo+1)
+        }
     }
+    console.log(imagensItens[2])
     return(
         <div>
             <button onClick={voltar}>{"<"}</button>
