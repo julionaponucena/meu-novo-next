1e008df59e42662102586ccf279728f6fabf1097
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-01-11 16:02:54 -0300

    Update janela-itens.js

--- supertags/janela-itens.js
+++ supertags/janela-itens.js
@@ -1,3 +1,118 @@
-const imagensItens = ['anuncio-curso.png','anuncio-pais.jpg','foto-2.jpeg','foto-3.jpeg','galera-agrupada.jpg','galera-assistindo.JPG','galera-circulo.jpg','galera-dialogo.JPG','galera-exercitando.jpg','galera-lanche.jpg','galera-linha2.jpg','galera-mesa.JPG','galera-roda.jpg','galera-vendada.jpg','moca-vendada.JPG','palestra.JPG','palestra2.JPG','palestra3.JPG','palestra4.jpg','praticando-atividade.jpg','professor-aula.JPG','professor-aula2.JPG','quadro-avisos.jpg','escrevendo-costa.jpg','aula-circulo.jpg','aula-circulo2.jpg','atividade-grupo2.jpg','galera-abracando.jpg','galera-circulo2.jpg']
+const imagensItens = [{
+    nome:'anuncio-curso.png',
+    alt:'curso de extensão: contribuição da psicanálise para a educação emocional do professor'
+},
+{
+    nome:'anuncio-pais.jpg',
+    alt:'dialogo com pais e educadores: contribuição da psicanálise para um desenvolvimento saudavel'
+},
+{
+    nome:'foto-2.jpeg',
+    alt:'Professor Aparecido participando de uma palestra'
+},
+{
+    nome:'foto-3.jpeg',
+    alt:'Professor Aparecido participando de uma palestra'
+},
+{
+    nome:'galera-agrupada.jpg',
+    alt:'professor Aparecido fazendo uma atividade junto com um grupo de pessoas'
+},
+{
+    nome:'galera-assistindo.JPG',
+    alt:'Pessoas assistindo um vídeo'
+},
+{
+    nome:'galera-circulo.jpg',
+    alt:'pessoas participando de uma atividade educativa do professor Aparecido'
+},
+{
+    nome:'galera-dialogo.JPG',
+    alt:'palestra do professor Aparecido'
+},
+{
+    nome:'galera-exercitando.jpg',
+    alt:'pessoas praticando uma atividade educativa'
+},
+{
+    nome:'galera-lanche.jpg',
+    alt:'pessoas tomando lanche'
+},
+{
+    nome:'galera-linha2.jpg',
+    alt:'pessoas em fileira fazendo uma atividade'
+},
+{
+    nome:'galera-mesa.JPG',
+    alt:'pessoas reunidas em grupo'
+},
+{
+    nome:'galera-roda.jpg',
+    alt:'pessoas em roda praticando atividade educativa com a utilização de bolinhas'
+},
+{
+    nome:'galera-vendada.jpg',
+    alt:'pessoas com os olhos vendados sentados em uma roda'
+},
+{
+    nome:'moca-vendada.JPG',
+    alt:'moça com os olhos vendados'
+},
+{
+    nome:'palestra.JPG',
+    alt:'Professor Aparecido dando uma palestra'
+},
+{
+    nome:'palestra2.JPG',
+    alt:'pessoas assitindo uma palestra do professor Aparecido'
+},
+{
+    nome:'palestra3.JPG',
+    alt:'Professor Aparecido dando uma palestra'
+},
+{
+    nome:'palestra4.jpg',
+    alt:'Professor Aparecido dando uma palestra'
+},
+{
+    nome:'praticando-atividade.jpg',
+    alt:'atividade com baldes'
+},
+{
+    nome:'professor-aula.JPG',
+    alt:'palestra com professor Aparecido'
+},
+{
+    nome:'professor-aula2.JPG',
+    alt:'aula do professor Aparecido'
+},
+{
+    nome:'quadro-avisos.jpg',
+    alt:'quadro de avisos'
+},
+{
+    nome:'escrevendo-costa.jpg',
+    alt:'pessoas escrevendo nas costa de outras'
+},
+{
+    nome:'aula-circulo.jpg',
+    alt:'pessoas assistindo aula em circulo'   
+},
+{
+    nome:'aula-circulo2.jpg',
+    alt:'professor Aparecido  segurando uma caixa'
+},
+{
+    nome:'atividade-grupo2.jpg',
+    alt:'grupo de pessoas praticando uma atividade'
+},
+{
+    nome:'galera-abracando.jpg',
+    alt:'pessoas se abraçando'
+},{
+    nome:'galera-circulo2.jpg',
+    alt:'bate-papo em roda'
+}
+]
 
 export default imagensItens
