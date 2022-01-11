a9798aeeb99caccfa16afa961d571b652c0935a3
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-01-11 16:17:50 -0300

    Update filosofia-itens.js

--- menu/filosofia-itens.js
+++ menu/filosofia-itens.js
@@ -1,67 +1,80 @@
 const artigoFilosofia = [{
     img:'timido2.jpg',
     titulo:'TIMIDEZ: CAUSAS E FORMAS DE SUPERAÇÃO',
-    link:'filosofia-1'
+    link:'filosofia-1',
+    alt:'onça pequena encolhida atrás de uma arvore'
 },
 {
     img:'tigre-raivoso.jpg',
     titulo:'SENTIR RAIVA  SEMPRE É NEGATIVO?',
-    link:'filosofia-2'
+    link:'filosofia-2',
+    alt:'onça rugindo'
 },
 {
     img:'hero.png',
     titulo:'A ARTE DE VIVER A ALEGRIA DE SER UMA PESSOA COM DIGNIDADE',
-    link:'filosofia-3'
+    link:'filosofia-3',
+    alt:'desenho de uma criança feliz voando com capa'
 },
 {
     img:'terapia2.png',
     titulo:'PSICOLOGIA DO INDIVÍDUO: CORPO E MENTE SÃO INSEPARÁVEIS',
-    link:'filosofia-4'
+    link:'filosofia-4',
+    alt:'mente em conexão com o coração'
 },
 {
     img:'yoga.jpg',
     titulo:'CONSELHOS DE SÓCRATES PARA PLENIFICAR NOSSA EXISTÊNCIA',
-    link:'filosofia-5'
+    link:'filosofia-5',
+    alt:'desenho de uma pessoa meditando em uma arvore'
 },
 {
     img:'esperanca.png',
     titulo:'CULTIVAR A ESPERANÇA PODE TE FORTALECER  DIANTE DAS DIFICULDADES',
-    link:'filosofia-6'
+    link:'filosofia-6',
+    alt:'bonecos felizes'
 },
 {
     img:'tiger.png',
     titulo:'RELAÇÃO ENTRE A AUTO IMAGEM E QUALIDADE DE VIDA',
-    link:'filosofia-7'
+    link:'filosofia-7',
+    alt:'desenho de um tigre animado'
 },
 {
     img:'planta.jpg',
     titulo:'BENEFÍCIOS DA ESPIRITUALIDADE PARA O BEM-ESTAR MENTAL',
-    link:'filosofia-8'
+    link:'filosofia-8',
+    alt:'três pedras estando uma em cima da outra'
 },
 {
     img:'angel.png',
     titulo:'DIFERENÇA ENTRE ÉTICA E MORAL',
-    link:'filosofia-9'
+    link:'filosofia-9',
+    alt:'desenho de um anjo do bem e um anjo do mal'
 },
 {
     img:'relogio3.jpg',
     titulo:'PLANEJAR PARA ECONOMIZAR TEMPO E ENERGIA',
-    link:'filosofia-10'
+    link:'filosofia-10',
+    alt:'relogio'
 },
 {
     img:'meditation.jpg',
     titulo:'Sugestões para uma Boa Tomada de Decisão',
-    link:'filosofia-11'
+    link:'filosofia-11',
+    alt:'paisagem de varias pedras e árvores'
 },
 {
     img:'self.jpg',
     titulo:'CONHEÇA A SI MESMO',
-    link:'filosofia-12'
+    link:'filosofia-12',
+    alt:'mascara olhando para si mesma'
 },
 {
     img:'remedio.png',
     titulo:'QUANDO O REMÉDIO CAUSA DOENÇAS',
-    link:'filosofia-13'
+    link:'filosofia-13',
+    alt:'varias capsulas de remedios formando o mapa mundi'
 }
 
 ]
