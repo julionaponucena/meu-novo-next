b97bd3cdac6434ca3f9ff15758eb9dded7abccb7
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-01-12 12:31:27 -0300

    Update menu-itens.js

--- menu/menu-itens.js
+++ menu/menu-itens.js
@@ -1,245 +1,284 @@
-
-
-
-
-
-
-
-
-
-var Artigo = [{
+const Artigo = [{
  img: 'vitima.jpg',
  titulo: "NÃO SEJA VITIMADO PELO DETERMINISMO EXISTENCIAL",
- link:"psicanalise-2"
+ link:"psicanalise-2",
+ alt:'pessoa com uma caixa de papelão com uma carinha triste'
 },
 {
- img:'crianca-feliz.jpg',
- titulo: "COLABORAÇÃO PARA O BEM-ESTAR COLETIVO E AUTORREALIZAÇÃO",
- link:'psicanalise-3'
+    img:'crianca-feliz.jpg',
+    titulo: "COLABORAÇÃO PARA O BEM-ESTAR COLETIVO E AUTORREALIZAÇÃO",
+    link:'psicanalise-3',
+    alt:'pessoa pulando com os braços abertos'
 },
-{img:'ansiedade.jpg',
-titulo:"TERAPIA COM PSICANÁLISE  PARA ENFRENTAR AS DIFICULDADES EMOCIONAIS",
-link:'psicanalise-4'
+{
+    img:'ansiedade.jpg',
+    titulo:"TERAPIA COM PSICANÁLISE  PARA ENFRENTAR AS DIFICULDADES EMOCIONAIS",
+    link:'psicanalise-4',
+    alt:'pessoa para baixo encostada na parede'
 },
 {
     img:'pintura.jpg',
     titulo:"PSICANÁLISE: CAMINHO PARA A RESSIGNIFICAÇÃO DO SOFRIMENTO",
-    link:"psicanalise-6"
+    link:"psicanalise-6",
+    alt:'pintura abstrata'
 },
 { 
     img:'gato-sorte.jpg',
     titulo:"COMO SENTIR QUE VOCÊ TEM VALOR",
-    link:"psicanalise-7"
+    link:"psicanalise-7",
+    alt:'gatinho da sorte'
 },
 {
     img:'autovalor.jpg',
     titulo:"REFLEXÃO SOBRE OS TRAUMAS E NOSSA CONDIÇÃO EXISTENCIAL",
-    link:"psicanalise-8"
+    link:"psicanalise-8",
+    alt:'pessoa acenando com um joia'
 },
 {
     img:'contemplacao.jpg',
     titulo:"MENTES SADIAS GERAM  CORPOS SAUDÁVEIS",
-    link:'psicanalise-9'
+    link:'psicanalise-9',
+    alt:'desenho de uma pessoa em conexão com a paz interior'
 },
 {
     img:'reflexão.jpeg',
     titulo:"BREVE REFLEXÃO SOBRE A REALIZAÇÃO HUMANA",
-    link:'psicanalise-10'
+    link:'psicanalise-10',
+    alt:'homem apreciando o mar'
 },
 { 
     img:'luto.jpg',
     titulo:'A DOR DO LUTO',
-    link:"psicanalise-11"
+    link:"psicanalise-11",
+    alt:'mulher triste'
 },
 {
     img:'perdao.jpg',
     titulo:'PERDÃO E AUTOPERDÃO: SEUS BENEFÍCIOS PARA A SAÚDE FÍSICA E MENTAL',
-    link:'psicanalise-12'
+    link:'psicanalise-12',
+    alt:'dois bonecos de papelão olhando um para o outro'
 },
 {
     img:'escuro.jpg',
     titulo:"A CEGUEIRA PROVOCADA PELO DESEJO DE NÃO ENXERGAR: VISÃO PSICANALÍTICA DA NEGAÇÃO:",
-    link:'psicanalise-13'
+    link:'psicanalise-13',
+    alt:'pessoa passeando em uma floresta muito escura'
 },
 {
     img:'inferior.jpg',
     titulo:'COMPLEXO DE INFERIORIDADE: AUTOAFIRMAÇÃO NÃO, AUTOACEITAÇÃO SIM',
-    link:'psicanalise-14'
+    link:'psicanalise-14',
+    alt:'pessoa encolhida na cama'
 },
 { 
     img:'relogio.jpg',
     titulo:'RELAÇÃO ENTRE PASSADO E PRESENTE NA TERAPIA PSICANALÍTICA',
-    link:'psicanalise-15'
+    link:'psicanalise-15',
+    alt:'relógio'
 },
 {
     img:'educacao.jpeg',
     titulo:'PRATIQUE EDUCAÇÃO COM BASE NO ENCORAJAMENTO',
-    link:'psicanalise-16'
+    link:'psicanalise-16',
+    alt:'duas crianças brincando'
 },
 {
     img:'critica.jpg',
     titulo:"SUPERIORIDADE DISFARÇADA EM FORMA DE CRÍTICAS E  ELOGIOS",
-    link:'psicanalise-17'
+    link:'psicanalise-17',
+    alt:'pessoa mal avaliando o outro'
 },
 {
     img:'pessoas.jpg',
-    titulo:'IDENTIFIQUE A CAUSA DOS PROBLEMAS  DE RELACIONAMENTOS INTERPESSOAIS.php',
+    titulo:'IDENTIFIQUE A CAUSA DOS PROBLEMAS  DE RELACIONAMENTOS INTERPESSOAIS',
     link:'psicanalise-18',
+    alt:'pessoas pulando felizes'
 },
 {
     img:'reconhecimento.png',
     titulo:'LIVRAR-SE DA NECESSIDADE DE RECONHECIMENTO PODE SER LIBERTADOR',
-    link:'psicanalise-19'
+    link:'psicanalise-19',
+    alt:'desenho de uma pessoa entregando o troféu para o outro'
 },
 {
     img:'festa.jpg',
     titulo:'O SOFRIMENTO A PARTIR DOS RELACIONAMENTOS INTERPESSOAIS',
     link:'psicanalise-20',
+    alt:'duas pessoas segurando uma vela de aniversário'
 },
 {
     img:'armadilha.png',
     titulo:'NÃO PERMITA QUE SUA VIDA SEJA DETERMINADA PELAS “ARMADILHAS CONCEITUAIS”',
-    link:'psicanalise-21'
+    link:'psicanalise-21',
+    alt:'desenho de um dinheiro servindo de isca para uma armadilha'
 },
 { 
     img:'boneco-triste.jpg',
     titulo:'DEPRESSÃO E FALTA DE SENTIDO EXISTENCIAL',
-    link:'psicanalise-22'
+    link:'psicanalise-22',
+    alt:'boneco de papelão triste'
 },
 {
     img:'interrogação.jpg',
     titulo:'DIMINUA A ANSIEDADE PARATICANDO A GESTÃO DE INCERTEZAS',
-    link:'psicanlise-23'
+    link:'psicanlise-23',
+    alt:'balões de interrogação'
 },
 {
     img:'oceano.jpg',
     titulo:'ADMITIR O ERRO NÃO É SINAL DE FRAQUEZA E MUITO MENOS DE DERROTA',
-    link:'psicanalise-24'
+    link:'psicanalise-24',
+    alt:'paisagem de um oceano com várias montanhas'
 },
 {
     img:'estrelas.jpg',
     titulo:'PREOCUPAÇÃO EXAGERADA COM A IMAGEM EXTERIOR PODE GERAR CONFLITO INTERIOR',
-    link:'psicanalise-25'
+    link:'psicanalise-25',
+    alt:'céu repleto de estrelas'
 },
 {
     img:'significar.jpg',
     titulo:'RESSIGNIFIQUE SEU OLHAR DIANTE DA VIDA E SE LIVRE DA COMPETIÇÃO COMPULSIVA',
-    link:'psicanalise-26'
+    link:'psicanalise-26',
+    alt:'pessoas brincando de acróstico'
 },
 { 
     img:'boxe.jpg',
     titulo:'POR QUE TRANSFORMAMOS A VIDA EM UMA GRANDE COMPETIÇÃO?',
-    link:'psicanalise-27'
+    link:'psicanalise-27',
+    alt:'duas pessoas dando as mãos com luvas de boxe'
 },
 {
     img:'rei.jpg',
     titulo:'RELAÇÃO ENTRE ARROGÂNCIA E COMPLEXO DE INFERIORIDADE',
-    link:'psicanalise-28'
+    link:'psicanalise-28',
+    alt:'peça de xadrez com uma coroa de rei em cima dela'
 },
 {
     img:'gatinho.jpg',
     titulo:'USE O SENTIMENTO DE INFERIORIDADE COMO SEU ALIADO',
-    link:'psicanalise-29'
+    link:'psicanalise-29',
+    alt:'gato encolhido atrás de uma parede'
 },
 {
     img:'psicoterapia.jpg',
     titulo:'A PRÁTICA DA PSICOTERAPIA E O CAMINHO DA CURA',
-    link:'psicanalise-30'
+    link:'psicanalise-30',
+    alt:'desenho de uma pessoa ajudando a resolver uma grande quantidade de pensamentos bagunçados da mente de outra pessoa'
 },
 {
     img:'coracao-partido.jpg',
     titulo:'O QUE EXISTE DE OBJETIVO NOS SENTIMENTOS DE INFERIORIDADE?',
-    link:'psicanalise-31'
+    link:'psicanalise-31',
+    alt:'boneco de papelão com o coração partido'
 },
 {
     img:'homem-ceu.jpg',
     titulo:'POR QUE MUITAS PESSOAS SE MALTRATAM APOSTANDO EM SEUS PRÓPRIOS "DEFEITOS"?',
-    link:'psicanalise-32'
+    link:'psicanalise-32',
+    alt:'pessoa apreciando a paisagem'
 },
 {
     img:'caminho.jpg',
     titulo:'SUA VIDA É DECIDIDA AQUI E AGORA',
-    link:'psicanalise-33'
+    link:'psicanalise-33',
+    alt:'pessoa caminhando'
 },
 {
     img:'mudanca.jpg',
     titulo:'POR QUE MUITOS OPTAM POR NÃO MUDAR?',
-    link:'psicanalise-34'
+    link:'psicanalise-34',
+    alt:'desenho de um peixe mudando para um aquário maior'
 },
 { 
     img:'psicoterapia-online.jpg',
     titulo:'Terapia Online para adultos, adolescentes e casais.',
-    link:'psicanalise-35'
+    link:'psicanalise-35',
+    alt:'dois bonecos dentro de um computador se comprimentando'
 },
 {
     img:'rocks.jpg',
     titulo:'VOCÊ SE SENTE BEM DO JEITO QUE ESTÁ?',
-    link:'psicanalise-36'
+    link:'psicanalise-36',
+    alt:'uma pedra em cima da outra'
 },
 {
     img:'raiva.jpg',
     titulo:'COMO LIDAR COM O SENTIMENTO DA RAIVA?',
-    link:'psicanalise-37'
+    link:'psicanalise-37',
+    alt:'tomate com raiva'
 },
 { 
     img:'relogio-2.jpg',
     titulo:'É POSSÍVEL VIVER SEM SER CONTROLADO PELO PASSADO?',
-    link:'psicanalise-38'
+    link:'psicanalise-38',
+    alt:'relógio antigo'
 },
 {
     img:'borboleta.jpg',
     titulo:'REFLEXÃO SOBRE OS TRAUMAS E NOSSA CONDIÇÃO EXISTENCIAL',
-    link:'psicanalise-39'
+    link:'psicanalise-39',
+    alt:'borboleta saindo do casulo'
 },
 {
     img:'praia.jpg',
     titulo:'RELAÇÃO ENTRE PASSADO E PRESENTE NA TERAPIA PSICANALÍTICA',
-    link:'psicanalise-40'
+    link:'psicanalise-40',
+    alt:'ponte'
 },
 {
     img:'coracao.jpg',
     titulo:'PSICANÁLISE: AJUDA PARA ENFRENTAR A DIFICULDADE DE INTERPRETAR NOSSOS SENTIMENTOS E EMOÇÕES',
-    link:'psicanalise-41'
+    link:'psicanalise-41',
+    alt:'folhas em formato de coração'
 },
 {
     img:'destino.jpg',
     titulo:'PSICANÁLISE PARA MUDANÇA NA “PREDESTINAÇÃO”',
-    link:'psicanalise-42'
+    link:'psicanalise-42',
+    alt:'estrada'
 },
 {
     img:'liberdade.jpg',
     titulo:'ANÁLISE: CAMINHO PARA LIVRAR-SE DE SINTOMAS INCÔMODOS',
-    link:'psicanalise-43'
+    link:'psicanalise-43',
+    alt:'pessoa curtindo a natureza'
 },
 {
     img:'libertar.jpg',
     titulo:'RAZÃO PARA FAZER ANÁLISE: SAIR DE UMA CONDIÇÃO DIFÍCIL',
-    link:'psicanalise-44'
+    link:'psicanalise-44',
+    alt:'pessoa caminhando na natureza'
 },
 {
     img:'lampada.jpg',
     titulo:'RAZÕES PARA FAZER TERAPIA 3',
-    link:'psicanalise-45'
+    link:'psicanalise-45',
+    alt:'lampada com desenho de vários balões saindo dela'
 },
 {
     img:'biblioteca.jpg',
     titulo:'RAZÕES PARA FAZER TERAPIA EM PSICANÁLISE 2:',
-    link:'psicanalise-46'
+    link:'psicanalise-46',
+    alt:'vários livros em uma prateleira'
 },
 {
     img:'conversa.jpg',
     titulo:'RAZÕES PARA FAZER TERAPIA, SEGUNDO JUNG',
-    link:'psicanalise-47'
+    link:'psicanalise-47',
+    alt:'dois balões de dialogo'
 },
 {
     img:'psicoterapia.jpg',
     titulo:'A PRÁTICA DA PSICOTERAPIA E O CAMINHO DA CURA',
-    link:'psicanalise-48'
+    link:'psicanalise-48',
+    alt:'desenho de uma pessoa ajudando a resolver uma grande quantidade de pensamentos bagunçados da mente de outra pessoa'
 },
 {
     img:'terapia.jpg',
     titulo:'POR QUE FAZER TERAPIA COM PSICANÁLISE?',
-    link:'psicanalise-49'
+    link:'psicanalise-49',
+    alt:'pessoa tendo terapia com um psicanalista'
 }
 ]
 
