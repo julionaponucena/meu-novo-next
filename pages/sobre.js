458ffcac6a0a94b0f1e0d8f7ec59f52a7d7705fb
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-01-30 19:26:56 -0300

    Update sobre.js

--- pages/sobre.js
+++ pages/sobre.js
@@ -23,7 +23,7 @@ function Sobre (){
     async function sendEmail (evt){
         evt.preventDefault()
         try{
-            const response = await fetch('https://localhost:8000/email/token',{
+            const response = await fetch('https://meu-django.herokuapp.com/email/token',{
                 method:'POST',
                 headers:{
                     Accept:'application/json',
