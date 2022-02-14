bba687eda383ab5e79188b591dbf195064e0b330
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-02-14 15:44:23 -0300

    Create generate-sitemap.js

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,14 @@
+const sitemap = require('nextjs-sitemap-generator')
+const path = require('path')
+
+sitemap({
+    baseUrl:'https://restauramente.com.br',
+    pagesDirectory:path.resolve(__dirname,'../pages/'),
+    targetDirectory: path.resolve(__dirname ,'../public/') ,
+    ignoredPaths:[ 
+        '_document',
+        '_app',
+        'email',
+        'hello'
+    ]
+})
