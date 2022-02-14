4311334cab26efe17db0a0438af4ecbd154e496d
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-02-14 16:05:45 -0300

    Update generate-sitemap.js

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -4,7 +4,7 @@ const path = require('path')
 sitemap({
     baseUrl:'https://www.restauramente.com.br',
     pagesDirectory:path.resolve(__dirname,'../pages/'),
-    targetDirectory: path.resolve(__dirname ,'../public/') ,
+    targetDirectory: path.resolve(__dirname ,'../pages/') ,
     ignoredPaths:[ 
         '_document',
         '_app',
