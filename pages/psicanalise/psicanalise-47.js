784390b2f2dbe702a84f6f2ee9e20ec7a6820e27
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-02-26 17:11:24 -0300

    Update psicanalise-47.js

--- pages/psicanalise/psicanalise-47.js
+++ pages/psicanalise/psicanalise-47.js
@@ -9,7 +9,7 @@ function Psicanalise_47 (){
     return (
         <>
         <Head>
-            <title></title>
+            <title>Razões para Fazer Terapia, Segundo Jung</title>
             <meta name="keywords" content="razões para fazer terapia"/>
             <meta name="description" content="Jung faz a seguinte afirmação:“No processo analítico constata-se um desenvolvimento, um progresso em direção a uma certa meta ou fim cuja natureza enigmática me ocupou durante anos a fio.”"/>
         </Head>
