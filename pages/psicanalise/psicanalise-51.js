822ddfab772865eba09b60ee1ca26b0d2d7419bc
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-03-13 21:11:52 -0300

    Create psicanalise-51.js

---
+++ pages/psicanalise/psicanalise-51.js
@@ -0,0 +1,28 @@
+import Head from "next/head"
+import Header from "../../include/header"
+import Menu from "../../include/menu"
+import Footer from "../../include/footer"
+import Principal from "../../supertags/principal"
+import Titulo from "../../supertags/titulo"
+
+function psicanalise_51 (){
+    return(
+        <>
+        <Head>
+            <title>Infelicidade: Escolha e Ganho Secundário</title>
+            <meta name="keywords" content="ganho secundario"/>
+            <meta name="description"content="Se pararmos para refletir um pouco que seja sobre a verdadeira felicidade e suas raízes, verificaremos que as pessoas que são de fato felizes, construíram essa condição percorrendo um longo caminho, durante o qual o principal investimento foi na consolidação do seu “eu,”  através de um trabalho de intensa construção e reconstrução interna..."/>
+        </Head>
+        <Header/>
+        <Menu/>
+        <Principal>
+            <Titulo>INFELICIDADE: ESCOLHA E GANHO SECUNDÁRIO</Titulo>
+            <p>É assunto quase unânime em nossa sociedade atual que aquilo que move a maioria das nossas ações é o desejo de nos tornarmos felizes. A grande demanda por uma vida feliz que nos impomos muitas vezes surte o efeito contrário, visto que idealizamos as mais diversas situações que poderiam nos conduzir à felicidade, sem no entanto nos questionarmos se estamos percorrendo o caminho certo e, sobretudo, se aquilo que estamos buscando reúne as condições para nos fazer felizes de fato.</p>
+            <p>Se pararmos para refletir um pouco que seja sobre a verdadeira felicidade e suas raízes, verificaremos que as pessoas que são de fato felizes, construíram essa condição percorrendo um longo caminho, durante o qual o principal investimento foi na consolidação do seu “eu,”  através de um trabalho de intensa construção e reconstrução interna. Quando se parte para uma busca pessoal sob essa perspectiva os elementos externos se tornam meros coadjuvantes que, embora tenham importância, não se constituem no elemento central da nossa existência. Quando nos negamos a ponderar sobre essa possibilidade, podemos estar diante de uma busca inconsciente por um fenômeno que em psicanálise chamamos de ganho secundário, diante do qual o indivíduo investe em  ações que lhe provocam sofrimento e infelicidade com o intuito de conseguir através deles alguma satisfação. Diante disso, podemos afirmar que a infelicidade passa a ser uma opção  que o indivíduo faz, porque de alguma forma considera que será bom para ele. Por aqui vemos o quanto é importante efetuarmos uma investigação profunda sobre nossa constituição psíquica e os atos dela resultantes. Você gostaria de saber como a psicanálise pode te ajudar nessa viagem para dentro de si mesmo? Entre em contato conosco.</p>
+        </Principal>
+        <Footer/>
+        </>
+    )
+}
+
+export default psicanalise_51;
