2545c15b983f93ea2cc3e44d4157c3455403e7d0
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-04-09 19:26:32 -0300

    Update curso-menu.js

--- pages/curso-menu.js
+++ pages/curso-menu.js
@@ -1,14 +1,17 @@
+import Head from "next/head"
 import HiperLink from "../scripts/hiperlinks"
 import Header from "../include/header"
 import Menu from "../include/menu"
 import Footer from "../include/footer"
 import artigoCursos from "../menu/curso-itens"
 import ContainerMenu from "../menu/container-menu";
-import HeadConfig from "../include/head";
+
 function CursoMenu (){
     return(
         <>
-            <HeadConfig/>
+            <Head>
+                <title>Cursos</title>
+            </Head>
             <Header/>
             <Menu/>
             <ContainerMenu titulo="CURSOS">
