e567bda5043450e42db07bec1597a7938a282468
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-10-04 16:01:41 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,9 @@ class MyDocument extends Document{
                     content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
                     />
                     <link rel="manifest" href="/manifest.json"/>
-                    <link rel="icon" href="/logo-mente.png"/>
+                    <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png"/>
+                    <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png"/>
+                    <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png"/>
                     <link rel="shortcut icon" href="/logo-mente.png" type="imagem/x-icon"/>
                     <meta name="author" content="Júlio Naponucena de Alcântara"/>
                 </Head>
