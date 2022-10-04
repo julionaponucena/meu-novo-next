619ee716d76adf44a4077441b0e925fea967f16c
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-10-04 14:40:06 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -5,6 +5,12 @@ class MyDocument extends Document{
         return(
             <Html Lang="pt-br">
                 <Head>
+                    <meta
+                    name='viewport'
+                    content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover'
+                    />
+                    <link rel="manifest" href="/manifest.json"/>
+                    <link rel="icon" href="/logo-mente.png"/>
                     <link rel="shortcut icon" href="/logo-mente.png" type="imagem/x-icon"/>
                     <meta name="author" content="Júlio Naponucena de Alcântara"/>
                 </Head>
