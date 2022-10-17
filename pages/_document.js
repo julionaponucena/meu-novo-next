5fe1340408a4ed02febeef62bbf1b74f49582eac
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-10-17 13:06:40 -0300

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html,Head,Main,NextScript } from "next/document";
 class MyDocument extends Document{
     render(){
         return(
-            <Html Lang="pt-br">
+            <Html lang="pt-br">
                 <Head>
                     <meta
                     name='viewport'
