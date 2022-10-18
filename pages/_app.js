a4b803266d640fa7cd62809cbb11c367cd362442
Júlio Alcântara <julio.naponucena@hotmail.com>
2022-10-18 12:48:29 -0300

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,7 @@
-import { useEffect } from 'react'
 import '../styles/main.css'
 import '../styles/menu.css'
 import '../styles/index.css'
 function MyApp({ Component, pageProps }) {
-  function registerPWA(){
-    if('serviceWorker' in navigator){
-
-      navigator.serviceWorker.register('/sw.js')
-    }
-  }
-  useEffect(registerPWA)
   return <Component {...pageProps} />
 }
 
