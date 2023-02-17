ff8c2848bd7f41dbf079531ba53d08773cc4f099
julioalcantara <julio.alcantara@ekaizen.digital>
2023-02-17 17:53:14 -0300

    create email backend service

--- pages/api/email.js
+++ pages/api/email.js
@@ -1,8 +1,27 @@
 const nodemailer = require('nodemailer')
+const mailgun = require('mailgun-js')
 
-function SendEmail (req,res){
-   
-    let transporte = nodemailer.createTransport({
+async function SendEmail (request,response){
+    const {nome,email,descricao} =request.body;
+    const domain =process.env.EMAIL_DOMAIN
+    const mg = mailgun({apiKey:process.env.API_EMAIL_KEY,domain:process.env.EMAIL_DOMAIN})
+    console.log(process.env.EMAIL_DOMAIN)
+    const data = {
+        from:`Júlio <${email}>`,
+        to: `${process.env.EMAILS_RECEIVERS}`,
+        subject:`email do Restauramente de ${nome}`,
+        text:'email de teste',
+      //  message:descricao,
+    };
+
+const emailResponse = await mg.messages().send(data,(error,body) =>{
+        console.log(error)
+        console.log(`body :${body}`)
+    })
+    console.log(`emailResponse: ${emailResponse}`)
+    response.status(200).send()
+
+    /*let transporte = nodemailer.createTransport({
         host:'smtp.office365.com',
         port:587,
         auth:{
@@ -17,7 +36,7 @@ function SendEmail (req,res){
         subject:'Contato atravez do site!',
         text:`<b>"teste"</b><br/>"teste"`,
         html:'<b>Olá!</b>'
-    }).then(response => {res.send(response)}).catch((erro =>{res.send(erro)}))
+    }).then(response => {res.send(response)}).catch((erro =>{res.send(erro)}))*/
     
 }
 export default SendEmail
