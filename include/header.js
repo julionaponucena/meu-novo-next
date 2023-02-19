d5b3fbe51c7a641a39e0fff8ed3d15b332ae14e2
julioalcantara <julio.alcantara@ekaizen.digital>
2023-02-19 20:27:50 -0300

    finish send email service

--- include/header.js
+++ include/header.js
@@ -1,33 +1,53 @@
-import Image from "next/image";
-import style from "../styles/modulos/header.module.css"
+import Image from 'next/image';
+import style from '../styles/modulos/header.module.css';
 
+function Header() {
+  return (
+    <header className={style.Header}>
+      <div className={style.cabecalho}>
+        <Image
+          className={style.logo}
+          src="/logo-mente.png"
+          width={90}
+          height={90}
+          alt="logo do site"
+          loading="eager"
+        />
 
-
-function Header (){
-    
-    return (
-        <header className={style.Header}>
-            <div className={style.cabecalho}> 
-                
-                    <Image className={style.logo} src="/logo-mente.png" width={90} height={90} alt="logo do site" loading="eager"/>
-                
-                <h1 className={style.titulo}>estauramente</h1>
-            
-            </div>
-            <div className={style.RedesSociais}>
-                <a className={style.redeLink} target="_blank" rel="norrefer external" href="https://www.instagram.com/psicanalistaaparecidoalcantara/">
-                    <Image src="/Instagram-Icon.png"width={40} height={40} alt="icone do instagram" loading="eager"/>
-                    
-                    
-                </a>
-                <a className={style.redeLink} target="_blank" rel="norrefer external" href="https://www.facebook.com/isaltosp" >
-                    <Image src="/facebook-icon.png"width={40} height={40} alt="icone do facebook" loading="eager" />
-                    
-                    
-                </a>
-            </div> 
-        </header>
-    )
+        <h1 className={style.titulo}>estauramente</h1>
+      </div>
+      <div className={style.RedesSociais}>
+        <a
+          className={style.redeLink}
+          target="_blank"
+          rel="norrefer external noreferrer"
+          href="https://www.instagram.com/terapia.aparecidoalcantara/"
+        >
+          <Image
+            src="/Instagram-Icon.png"
+            width={40}
+            height={40}
+            alt="icone do instagram"
+            loading="eager"
+          />
+        </a>
+        <a
+          className={style.redeLink}
+          target="_blank"
+          rel="norrefer external noreferrer"
+          href="https://www.facebook.com/isaltosp"
+        >
+          <Image
+            src="/facebook-icon.png"
+            width={40}
+            height={40}
+            alt="icone do facebook"
+            loading="eager"
+          />
+        </a>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
--- pages/api/email.js
+++ pages/api/email.js
@@ -1,42 +1,26 @@
 const nodemailer = require('nodemailer')
-const mailgun = require('mailgun-js')
 
 async function SendEmail (request,response){
-    const {nome,email,descricao} =request.body;
-    const domain =process.env.EMAIL_DOMAIN
-    const mg = mailgun({apiKey:process.env.API_EMAIL_KEY,domain:process.env.EMAIL_DOMAIN})
-    console.log(process.env.EMAIL_DOMAIN)
-    const data = {
-        from:`Júlio <${email}>`,
-        to: `${process.env.EMAILS_RECEIVERS}`,
-        subject:`email do Restauramente de ${nome}`,
-        text:'email de teste',
-      //  message:descricao,
-    };
+    const {nome,email,descricao} = request.body
 
-const emailResponse = await mg.messages().send(data,(error,body) =>{
-        console.log(error)
-        console.log(`body :${body}`)
-    })
-    console.log(`emailResponse: ${emailResponse}`)
-    response.status(200).send()
-
-    /*let transporte = nodemailer.createTransport({
-        host:'smtp.office365.com',
+    const transporte = nodemailer.createTransport({
+        service:'gmail',
         port:587,
         auth:{
             user:process.env.USERMAIL,
             pass:process.env.PASSMAIL
         }
     })
-    transporte.sendMail({
-        from:`"como criar" <${process.env.USERMAIL}>`,
-        from:process.env.USERMAIL,
-        replyTo:process.env.USERMAIL,
-        subject:'Contato atravez do site!',
-        text:`<b>"teste"</b><br/>"teste"`,
-        html:'<b>Olá!</b>'
-    }).then(response => {res.send(response)}).catch((erro =>{res.send(erro)}))*/
+
     
+    await transporte.sendMail({
+        from:email,
+        to:process.env.RECEIVERS,
+        subject:`Mensagem de ${nome} do Restauramente`,
+        text:descricao
+    })
+    
+
+    response.status(200).send()
 }
 export default SendEmail
